096f1ece03dd8efdaee11b2ccf16b9d5fb3a7f41
Matt <mateusz@hi-arte.pl>
2019-01-24 13:06:20 +0100

    Fix ec-component-icon compatible with suggestions from pull request #12

--- src/ec/packages/ec-component-icon/icon.story.js
+++ src/ec/packages/ec-component-icon/icon.story.js
@@ -1,252 +1,142 @@
 import { storiesOf } from '@storybook/html';
 import { withKnobs, select } from '@storybook/addon-knobs';
+import { withNotes } from '@ecl-twig/storybook-addon-notes';
+import withCode from '@ecl-twig/storybook-addon-code';
+
+import brandedIcons from '@ecl/ec-resources-icons/dist/lists/branded.json';
+import generalIcons from '@ecl/ec-resources-icons/dist/lists/general.json';
+import notificationsIcons from '@ecl/ec-resources-icons/dist/lists/notifications.json';
+import uiIcons from '@ecl/ec-resources-icons/dist/lists/ui.json';
+
+import iconDocs from './docs/icon.md';
 
 import icon from './icon.html.twig';
 
+const sizes = {
+  XS: 'xs',
+  S: 's',
+  M: 'm',
+  L: 'l',
+  XL: 'xl',
+  '2XL': '2xl',
+};
+
+const defaultSize = 'm';
+
+const colors = {
+  Default: '',
+  Inverted: 'inverted',
+  Primary: 'primary',
+};
+
+const defaultColor = '';
+
+const transforms = {
+  None: '',
+  'Rotate 90': 'rotate-90',
+  'Rotate 180': 'rotate-180',
+  'Rotate 270': 'rotate-270',
+  'Flip horizontal': 'flip-horizontal',
+  'Flip vertical': 'flip-vertical',
+};
+
+const defaultTransform = '';
+
+const defaultIconPath = '/static/media/icons.cbfd6efe.svg';
+
 storiesOf('Components/Icon', module)
   .addDecorator(withKnobs)
-  .add('branded', () =>
-    icon({
-      icon: {
-        type: 'branded',
-        name: select(
-          'Icon',
-          {
-            Facebook: 'facebook',
-            'Google Plus': 'google-plus',
-            Instagram: 'instagram',
-            'Linked In': 'linkedin',
-            Pinterest: 'pinterest',
-            Rss: 'rss',
-            Skype: 'skype',
-            Twitter: 'twitter',
-            Youtube: 'youtube',
-          },
-          'facebook'
-        ),
-        size: select(
-          'Size',
-          {
-            XS: 'xs',
-            S: 's',
-            M: 'm',
-            L: 'l',
-            XL: 'xl',
-            '2XL': '2xl',
-            Fluid: 'fluid',
-          },
-          'm'
-        ),
-        transform: select(
-          'Transform',
-          {
-            None: 'rotate-0',
-            'Rotate 90': 'rotate-90',
-            'Rotate 180': 'rotate-180',
-            'Rotate 270': 'rotate-270',
-            'Flip Horizontal': 'flip-horizontal',
-            'Flip Vertical': 'flip-vertical',
-          },
-          'rotate-0'
-        ),
-        color: select(
-          'Color',
-          {
-            Default: '',
-            Inverted: 'inverted',
-            Primary: 'primary',
-          },
-          ''
-        ),
-      },
-    })
+  .addDecorator(withNotes)
+  .addDecorator(withCode)
+  .add(
+    'branded',
+    () => {
+      const shape = select('Icon', brandedIcons, brandedIcons[0]);
+      const size = select('Size', sizes, defaultSize);
+      const color = select('Color', colors, defaultColor);
+      const transform = select('Transform', transforms, defaultTransform);
+
+      return icon({
+        icon: {
+          type: 'branded',
+          name: shape,
+          size,
+          transform,
+          color,
+          path: defaultIconPath,
+        },
+      });
+    },
+    {
+      notes: { markdown: iconDocs },
+    }
   )
-  .add('general', () =>
-    icon({
-      icon: {
-        type: 'general',
-        name: select(
-          'Icon',
-          {
-            audio: 'audio',
-            book: 'book',
-            brochure: 'brochure',
-            budget: 'budget',
-            calendar: 'calendar',
-            copy: 'copy',
-            data: 'data',
-            digital: 'digital',
-            edit: 'edit',
-            energy: 'energy',
-            euro: 'euro',
-            faq: 'faq',
-            feedback: 'feedback',
-            file: 'file',
-            gear: 'gear',
-            'generic-lang': 'generic-lang',
-            global: 'global',
-            growth: 'growth',
-            image: 'image',
-            infographic: 'infographic',
-            language: 'language',
-            livestreaming: 'livestreaming',
-            location: 'location',
-            'multiple-files': 'multiple-files',
-            organigram: 'organigram',
-            package: 'package',
-            presentation: 'presentation',
-            print: 'print',
-            regulation: 'regulation',
-            search: 'search',
-            share: 'share',
-            spinner: 'spinner',
-            spreadsheet: 'spreadsheet',
-            video: 'video',
-          },
-          'audio'
-        ),
-        size: select(
-          'Size',
-          {
-            XS: 'xs',
-            S: 's',
-            M: 'm',
-            L: 'l',
-            XL: 'xl',
-            '2XL': '2xl',
-            Fluid: 'fluid',
-          },
-          'm'
-        ),
-        transform: select(
-          'Transform',
-          {
-            None: 'rotate-0',
-            'Rotate 90': 'rotate-90',
-            'Rotate 180': 'rotate-180',
-            'Rotate 270': 'rotate-270',
-            'Flip Horizontal': 'flip-horizontal',
-            'Flip Vertical': 'flip-vertical',
-          },
-          'rotate-0'
-        ),
-        color: select(
-          'Color',
-          {
-            Default: '',
-            Inverted: 'inverted',
-            Primary: 'primary',
-          },
-          ''
-        ),
-      },
-    })
+  .add(
+    'general',
+    () => {
+      const shape = select('Icon', generalIcons, generalIcons[0]);
+      const size = select('Size', sizes, defaultSize);
+      const color = select('Color', colors, defaultColor);
+      const transform = select('Transform', transforms, defaultTransform);
+
+      return icon({
+        icon: {
+          type: 'general',
+          name: shape,
+          size,
+          transform,
+          color,
+          path: defaultIconPath,
+        },
+      });
+    },
+    {
+      notes: { markdown: iconDocs },
+    }
   )
-  .add('notifications', () =>
-    icon({
-      icon: {
-        type: 'notifications',
-        name: select(
-          'Icon',
-          {
-            error: 'error',
-            information: 'information',
-            success: 'success',
-            warning: 'warning',
-          },
-          'error'
-        ),
-        size: select(
-          'Size',
-          {
-            XS: 'xs',
-            S: 's',
-            M: 'm',
-            L: 'l',
-            XL: 'xl',
-            '2XL': '2xl',
-            Fluid: 'fluid',
-          },
-          'm'
-        ),
-        transform: select(
-          'Transform',
-          {
-            None: 'rotate-0',
-            'Rotate 90': 'rotate-90',
-            'Rotate 180': 'rotate-180',
-            'Rotate 270': 'rotate-270',
-            'Flip Horizontal': 'flip-horizontal',
-            'Flip Vertical': 'flip-vertical',
-          },
-          'rotate-0'
-        ),
-        color: select(
-          'Color',
-          {
-            Default: '',
-            Inverted: 'inverted',
-            Primary: 'primary',
-          },
-          ''
-        ),
-      },
-    })
+  .add(
+    'notifications',
+    () => {
+      const shape = select('Icon', notificationsIcons, notificationsIcons[0]);
+      const size = select('Size', sizes, defaultSize);
+      const color = select('Color', colors, defaultColor);
+      const transform = select('Transform', transforms, defaultTransform);
+
+      return icon({
+        icon: {
+          type: 'notifications',
+          name: shape,
+          size,
+          transform,
+          color,
+          path: defaultIconPath,
+        },
+      });
+    },
+    {
+      notes: { markdown: iconDocs },
+    }
   )
-  .add('ui', () =>
-    icon({
-      icon: {
-        type: 'ui',
-        name: select(
-          'Icon',
-          {
-            'check-filled': 'check-filled',
-            check: 'check',
-            'close-filled': 'close-filled',
-            close: 'close',
-            'corner-arrow': 'corner-arrow',
-            download: 'download',
-            external: 'external',
-            fullscreen: 'fullscreen',
-            'rounded-arrow': 'rounded-arrow',
-            'solid-arrow': 'solid-arrow',
-          },
-          'check-filled'
-        ),
-        size: select(
-          'Size',
-          {
-            XS: 'xs',
-            S: 's',
-            M: 'm',
-            L: 'l',
-            XL: 'xl',
-            '2XL': '2xl',
-            Fluid: 'fluid',
-          },
-          'm'
-        ),
-        transform: select(
-          'Transform',
-          {
-            None: 'rotate-0',
-            'Rotate 90': 'rotate-90',
-            'Rotate 180': 'rotate-180',
-            'Rotate 270': 'rotate-270',
-            'Flip Horizontal': 'flip-horizontal',
-            'Flip Vertical': 'flip-vertical',
-          },
-          'rotate-0'
-        ),
-        color: select(
-          'Color',
-          {
-            Default: '',
-            Inverted: 'inverted',
-            Primary: 'primary',
-          },
-          ''
-        ),
-      },
-    })
+  .add(
+    'ui',
+    () => {
+      const shape = select('Icon', uiIcons, uiIcons[0]);
+      const size = select('Size', sizes, defaultSize);
+      const color = select('Color', colors, defaultColor);
+      const transform = select('Transform', transforms, defaultTransform);
+
+      return icon({
+        icon: {
+          type: 'ui',
+          name: shape,
+          size,
+          transform,
+          color,
+          path: defaultIconPath,
+        },
+      });
+    },
+    {
+      notes: { markdown: iconDocs },
+    }
   );
---
+++ src/ec/packages/ec-component-icon/icon.test.js
@@ -0,0 +1,83 @@
+import path from 'path';
+import { renderTwigFile } from '@ecl-twig/test-utils';
+
+import brandedIcons from '@ecl/ec-resources-icons/dist/lists/branded.json';
+import generalIcons from '@ecl/ec-resources-icons/dist/lists/general.json';
+import notificationsIcons from '@ecl/ec-resources-icons/dist/lists/notifications.json';
+import uiIcons from '@ecl/ec-resources-icons/dist/lists/ui.json';
+
+describe('EC - Icon', () => {
+  const template = path.resolve(__dirname, './icon.html.twig');
+  const defaultPath = '/static/media/icons.cbfd6efe.svg';
+  const defaultDataStructure = {
+    icon: {
+      name: '',
+      type: '',
+      path: defaultPath,
+    },
+  };
+
+  describe('Branded', () => {
+    brandedIcons.forEach(icon => {
+      test(`- icon ${icon} renders correctly`, done => {
+        expect.assertions(1);
+
+        defaultDataStructure.name = icon;
+        defaultDataStructure.type = 'branded';
+
+        renderTwigFile(template, defaultDataStructure, (err, html) => {
+          expect(html).toMatchSnapshot();
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Notifications', () => {
+    notificationsIcons.forEach(icon => {
+      test(`- icon ${icon} renders correctly`, done => {
+        expect.assertions(1);
+
+        defaultDataStructure.name = icon;
+        defaultDataStructure.type = 'notifications';
+
+        renderTwigFile(template, defaultDataStructure, (err, html) => {
+          expect(html).toMatchSnapshot();
+          done();
+        });
+      });
+    });
+  });
+
+  describe('General', () => {
+    generalIcons.forEach(icon => {
+      test(`- icon ${icon} renders correctly`, done => {
+        expect.assertions(1);
+
+        defaultDataStructure.name = icon;
+        defaultDataStructure.type = 'general';
+
+        renderTwigFile(template, defaultDataStructure, (err, html) => {
+          expect(html).toMatchSnapshot();
+          done();
+        });
+      });
+    });
+  });
+
+  describe('UI', () => {
+    uiIcons.forEach(icon => {
+      test(`- icon ${icon} renders correctly`, done => {
+        expect.assertions(1);
+
+        defaultDataStructure.name = icon;
+        defaultDataStructure.type = 'ui';
+
+        renderTwigFile(template, defaultDataStructure, (err, html) => {
+          expect(html).toMatchSnapshot();
+          done();
+        });
+      });
+    });
+  });
+});
