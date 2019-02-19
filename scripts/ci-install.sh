bb6c284e4b3622d2bdadd793b48c4121f828b4b7
Yannick Huard <mail@yannickhuard.com>
2019-02-19 15:28:06 +0100

    chore(ci): test yarn.lock

---
+++ scripts/ci-install.sh
@@ -0,0 +1,18 @@
+# Exit the script on any command with non 0 return code
+set -e
+
+# Echo every command being executed
+set -x
+
+# Go to project root
+cd "$(dirname "$0")"
+cd ..
+
+# Install dependencies with yarn
+yarn install
+
+# Make sure git is clean (no changes to yarn.lock)
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Your git status is not clean. Please update yarn.lock. Aborting.";
+  exit 1;
+fi
