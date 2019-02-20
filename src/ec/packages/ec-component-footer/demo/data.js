c6a5733b29780dafd5cc1cf31e9645dc9e161b7e
Romain Emery <emery.romain@gmail.com>
2019-02-20 14:25:28 +0100

    feat: EC footer (#23)
    * add footer
    
    * update tests
    
    * fix test

---
+++ src/ec/packages/ec-component-footer/demo/data.js
@@ -0,0 +1,217 @@
+/* eslint-disable import/no-extraneous-dependencies */
+export const backToTop = {
+  link: {
+    label: 'Go to top',
+    path: '#top',
+  },
+  icon: {
+    path: 'static/icons.svg',
+    size: 'fluid',
+  },
+};
+
+export const identity = {
+  title: 'Site identification',
+  follow: {
+    label: 'Follow us:',
+    links: [
+      {
+        link: {
+          label: 'Facebook',
+          path: '/example',
+          icon_position: 'before',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'branded',
+          name: 'facebook',
+        },
+      },
+      {
+        link: {
+          label: 'Twitter',
+          path: '/example',
+          icon_position: 'before',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'branded',
+          name: 'twitter',
+        },
+      },
+      {
+        link: {
+          label: 'Other social networks',
+          path: '/example',
+          icon_position: 'before',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'ui',
+          name: 'external',
+        },
+      },
+    ],
+  },
+  info: [
+    {
+      link: {
+        label: 'Contact',
+        path: '/example',
+      },
+    },
+    {
+      link: {
+        label: 'Sitemap',
+        path: '/example',
+      },
+    },
+    {
+      link: {
+        label: 'Lorem ipsum',
+        path: '/example',
+      },
+    },
+    {
+      link: {
+        label: 'Lorem ipsum dolor sit',
+        path: '/example',
+      },
+    },
+  ],
+};
+
+export const sections = [
+  {
+    title: 'European Commission',
+    links: [
+      {
+        link: {
+          label: 'Commission and its priorities',
+          path: '/example',
+        },
+      },
+      {
+        link: {
+          label: 'Policies, information and services',
+          path: '/example',
+        },
+      },
+    ],
+  },
+  {
+    title: 'Follow the European Commission',
+    links: [
+      {
+        link: {
+          label: 'Facebook',
+          path: '/example',
+          icon_position: 'before',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'branded',
+          name: 'facebook',
+        },
+      },
+      {
+        link: {
+          label: 'Twitter',
+          path: '/example',
+          icon_position: 'before',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'branded',
+          name: 'twitter',
+        },
+      },
+      {
+        link: {
+          label: 'Other social networks',
+          path: '/example',
+          icon_position: 'before',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'ui',
+          name: 'external',
+        },
+      },
+    ],
+  },
+  {
+    title: 'European Union',
+    links: [
+      {
+        link: {
+          label: 'European Union',
+          path: '/example',
+          icon_position: 'after',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'ui',
+          name: 'external',
+        },
+      },
+      {
+        link: {
+          label: 'EU institutions',
+          path: '/example',
+          icon_position: 'after',
+        },
+        icon: {
+          path: 'static/icons.svg',
+          type: 'ui',
+          name: 'external',
+        },
+      },
+    ],
+  },
+];
+
+export const common = [
+  {
+    link: {
+      label: "About the Commission's new web presence",
+      path: '/example',
+    },
+  },
+  {
+    link: {
+      label: 'Language policy',
+      path: '/example',
+    },
+  },
+  {
+    link: {
+      label: 'Resources for partners',
+      path: '/example',
+    },
+  },
+  {
+    link: {
+      label: 'Cookies',
+      path: '/example',
+    },
+  },
+  {
+    link: {
+      label: 'Privacy policy',
+      path: '/example',
+    },
+  },
+  {
+    link: {
+      label: 'Legal notice',
+      path: '/example',
+    },
+  },
+  {
+    link: {
+      label: 'Contact',
+      path: '/example',
+    },
+  },
+];
