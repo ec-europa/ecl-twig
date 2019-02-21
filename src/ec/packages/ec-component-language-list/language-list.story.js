6714c0e7464482da4c81d04b613ff3cda218f479
emeryro <emery.romain@gmail.com>
2019-02-21 15:03:28 +0100

    fix notes

--- src/ec/packages/ec-component-language-list/language-list.story.js
+++ src/ec/packages/ec-component-language-list/language-list.story.js
@@ -6,6 +6,8 @@ import withCode from '@ecl-twig/storybook-addon-code';
 import logo from '@ecl/ec-resources-logo/logo--mute.svg';
 
 import { common, items, splash, overlay } from './demo/data';
+import splashDocs from './docs/splash.md';
+import overlayDocs from './docs/overlay.md';
 
 import languageList from './language-list.html.twig';
 
@@ -13,23 +15,33 @@ storiesOf('Components/Language list', module)
   .addDecorator(withKnobs)
   .addDecorator(withCode)
   .addDecorator(withNotes)
-  .add('splash', () =>
-    languageList({
-      items,
-      overlay: splash.overlay,
-      logo: {
-        alt: splash.logoAlt,
-        path: logo,
-      },
-      icon_path: common.iconPath,
-    })
+  .add(
+    'splash',
+    () =>
+      languageList({
+        items,
+        overlay: splash.overlay,
+        logo: {
+          alt: splash.logoAlt,
+          path: logo,
+        },
+        icon_path: common.iconPath,
+      }),
+    {
+      notes: { markdown: splashDocs },
+    }
   )
-  .add('overlay', () =>
-    languageList({
-      items,
-      overlay: overlay.overlay,
-      close_label: overlay.closeLabel,
-      title: overlay.title,
-      icon_path: common.iconPath,
-    })
+  .add(
+    'overlay',
+    () =>
+      languageList({
+        items,
+        overlay: overlay.overlay,
+        close_label: overlay.closeLabel,
+        title: overlay.title,
+        icon_path: common.iconPath,
+      }),
+    {
+      notes: { markdown: overlayDocs },
+    }
   );
