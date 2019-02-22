d6dd7777f114c4a8c98e62f33f590772f59af1fb
emeryro <emery.romain@gmail.com>
2019-02-22 09:16:42 +0100

    test code formatting

--- utils/storybook-addon-notes/src/register.jsx
+++ utils/storybook-addon-notes/src/register.jsx
@@ -47,10 +47,7 @@ class Notes extends React.Component {
     const { active } = this.props;
     const { text } = this.state;
     const textAfterFormatted = text
-      ? text
-          .trim()
-          .replace(/(<\S+.*>)\n/g, '$1')
-          .replace(/\n/g, '<br />')
+      ? text.replace(/(<\S+.*>)\n/g, '$1').replace(/\n/g, '<br />')
       : '';
 
     return active ? (
