93e0207394e834dbae87e2ed59c3c8fe0b091cab
Kalin Chernev <kalin.chernev@gmail.com>
2019-02-25 17:20:34 +0200

    Add demo data

---
+++ src/ec/packages/ec-component-social-media-follow/demo/data.js
@@ -0,0 +1,77 @@
+const demoData = {
+  description:
+    'Follow the latest progress and learn more about getting involved.',
+  links: [
+    {
+      path: '/example',
+      label: 'Twitter',
+      type: 'standalone',
+      icon_position: 'before',
+      icon: [
+        {
+          name: 'twitter',
+          size: 'xl',
+        },
+        {
+          name: 'twitter_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      path: '/example',
+      label: 'Facebook',
+      type: 'standalone',
+      icon_position: 'before',
+      icon: [
+        {
+          name: 'facebook',
+          size: 'xl',
+        },
+        {
+          name: 'facebook_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      path: '/example',
+      label: 'Google+',
+      type: 'standalone',
+      icon_position: 'before',
+      icon: [
+        {
+          name: 'gplus',
+          size: 'xl',
+        },
+        {
+          name: 'gplus_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      path: '/example',
+      label: 'Linkedin',
+      type: 'standalone',
+      icon_position: 'before',
+      icon: [
+        {
+          name: 'linkedin',
+          size: 'xl',
+        },
+        {
+          name: 'linkedin_hover',
+          size: 'xl',
+        },
+      ],
+    },
+    {
+      path: '/example',
+      label: 'Other social networks',
+      type: 'standalone',
+    },
+  ],
+};
+
+export default demoData;
--- src/ec/packages/ec-component-social-media-follow/social-media-follow.story.js
+++ src/ec/packages/ec-component-social-media-follow/social-media-follow.story.js
@@ -3,7 +3,7 @@ import { withKnobs, text } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
 
-import data from '@ecl/ec-specs-social-media-follow/demo/data';
+import data from './demo/data';
 
 import SocialMediaFollow from './social-media-follow.html.twig';
 import notes from './README.md';
