db86360dccd7fd6ec6e71d44a13560fda4930945
Matt <mateusz@hi-arte.pl>
2019-02-26 13:48:14 +0100

    Fix some bugs

--- src/ec/packages/ec-component-site-header/site-header.test.js
+++ src/ec/packages/ec-component-site-header/site-header.test.js
@@ -5,14 +5,35 @@ describe('EC - Site Header', () => {
   const template = path.resolve(__dirname, './site-header.html.twig');
   const defaultIconPath = 'static/icons.svg';
 
-  describe('Site Header default', () => {
+  describe('default', () => {
     test(`- renders correctly`, done => {
       expect.assertions(1);
 
       const data = {
         icon_file_path: defaultIconPath,
+        language: {
+          url: '/example',
+          code: 'en',
+          label: 'English',
+        },
         header_link: {
           url: '/example-1',
+          aria_label: 'European Commission',
+        },
+        header_image: {
+          src: 'static/media/logo--en.30b933cc.svg',
+          alt: 'European Commission logo',
+          title: 'European Commission',
+        },
+        search_form: {
+          button: {
+            label: 'Search',
+            icon: {
+              type: 'general',
+              name: 'search',
+              path: defaultIconPath,
+            },
+          },
         },
       };
 
@@ -23,7 +44,7 @@ describe('EC - Site Header', () => {
     });
   });
 
-  describe('Site Header translated', () => {
+  describe('translated', () => {
     test(`- renders correctly`, done => {
       expect.assertions(1);
 
@@ -46,6 +67,11 @@ describe('EC - Site Header', () => {
         search_form: {
           button: {
             label: 'Recherche',
+            icon: {
+              type: 'general',
+              name: 'search',
+              path: defaultIconPath,
+            },
           },
         },
       };
