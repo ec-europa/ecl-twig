08eb9ce77926e6cc9b17637322a0c86cb47bd22b
Yannick Huard <mail@yannickhuard.com>
2019-02-27 15:32:44 +0100

    correctly show markup

--- utils/storybook-addon-code/src/index.js
+++ utils/storybook-addon-code/src/index.js
@@ -7,16 +7,21 @@ const withCode = makeDecorator({
     const channel = addons.getChannel();
     const story = getStory(context);
 
-    if (typeof story === 'string' || story instanceof String) {
-      const formattedCode = beautifyHtml(story, {
-        indent_size: 2,
-        max_preserve_newlines: -1,
-        preserve_newlines: false,
-        indent_scripts: 'normal',
-      });
-
-      channel.emit('ecl/code/add_code', formattedCode);
+    let code = '';
+    if (typeof story === 'string') {
+      code = story;
+    } else if (story instanceof Node) {
+      code = story.outerHTML;
     }
+
+    const formattedCode = beautifyHtml(code, {
+      indent_size: 2,
+      max_preserve_newlines: -1,
+      preserve_newlines: false,
+      indent_scripts: 'normal',
+    });
+
+    channel.emit('ecl/code/add_code', formattedCode);
 
     return story;
   },
