0edcd99783e8403541b6feededa0b7a69a08f50c
Romain Emery <emery.romain@gmail.com>
2019-03-01 09:57:18 +0100

    feat: EC component select - INNO-1340 (#42)
    * init component
    
    * update component
    
    * whitespaces
    
    * update test
    
    * eye candy
    
    * update tests
    
    * add dash
    
    * update snap

---
+++ src/ec/packages/ec-component-select/select.story.js
@@ -0,0 +1,34 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { withNotes } from '@ecl-twig/storybook-addon-notes';
+import withCode from '@ecl-twig/storybook-addon-code';
+
+import specData from '@ecl/ec-specs-select/demo/data';
+import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
+
+import selectDocs from './docs/select.md';
+import select from './select.html.twig';
+
+storiesOf('Components/Select', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withNotes)
+  .addDecorator(withCode)
+  .add(
+    'default',
+    () =>
+      select({
+        label: text('Label', specData.label),
+        options: specData.options,
+        invalid: boolean('Invalid', false),
+        invalid_text: text('Error message', 'Error message'),
+        helper_text: text('Help message', 'Help message'),
+        disabled: boolean('Disabled', false),
+        hide_label: boolean('Hide label', false),
+        id: 'example-id',
+        name: 'example-name',
+        icon_path: defaultSprite,
+      }),
+    {
+      notes: { markdown: selectDocs },
+    }
+  );
