d69d230a6dc582d12acab27beeb51e3a1e607db0
Kalin Chernev <kalin.chernev@gmail.com>
2019-03-04 14:00:47 +0200

    Generate snapshot

--- src/ec/packages/ec-component-accordion/accordion.test.js
+++ src/ec/packages/ec-component-accordion/accordion.test.js
@@ -3,6 +3,10 @@ import { renderTwigFileAsNode } from '@ecl-twig/test-utils';
 
 import demoData from './demo/data';
 
+demoData.items.forEach(item => {
+  item.toggle.icon.path = 'example'; // eslint-disable-line no-param-reassign
+});
+
 describe('EC - Accordion', () => {
   const template = path.resolve(__dirname, './accordion.html.twig');
   const render = params => renderTwigFileAsNode(template, params);
