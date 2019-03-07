b8b32f5b20251d73e16e680847a232caab818d02
planctus <davidepuntoferrante@gmail.com>
2019-03-07 14:42:55 +0100

    feat(media-container) - Adding extra tests, removing dashes from control structures

--- src/ec/packages/ec-component-media-container/media-container.test.js
+++ src/ec/packages/ec-component-media-container/media-container.test.js
@@ -1,33 +1,60 @@
 import path from 'path';
-import { renderTwigFile } from '@ecl-twig/test-utils';
+import { merge, renderTwigFileAsNode } from '@ecl-twig/test-utils';
+
 import demoContent from '@ecl/ec-specs-media-container/demo/data--video';
 
-describe('EC - Media container image', () => {
+describe('EC Media Container', () => {
   const template = path.resolve(__dirname, './media-container.html.twig');
-  test(`- Media container image renders correctly`, done => {
-    expect.assertions(1);
-
-    const defaultDataStructure = {
+  const render = params => renderTwigFileAsNode(template, params);
+  const defaultDataStructure = {
+    mediaContainer: {
       description: demoContent.description,
       image: demoContent.image,
       alt: demoContent.alt,
-    };
+    },
+  };
 
-    renderTwigFile(template, defaultDataStructure, (err, html) => {
-      expect(html).toMatchSnapshot();
-      done();
+  describe('Generic tests', () => {
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const optionsWithExtraClasses = merge(defaultDataStructure, {
+        extra_classes: 'custom-media-container custom-media-container--test',
+      });
+
+      return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const optionsWithExtraClasses = merge(defaultDataStructure, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
     });
   });
-});
 
-describe('EC - Media container video', () => {
-  const template = path.resolve(__dirname, './media-container.html.twig');
-  test(`- Media container video renders correctly`, done => {
-    expect.assertions(1);
+  describe('EC - Media container image', () => {
+    test('- Media container image renders correctly', () => {
+      expect.assertions(1);
+      return expect(render(defaultDataStructure)).resolves.toMatchSnapshot();
+    });
+  });
 
-    renderTwigFile(template, demoContent, (err, html) => {
-      expect(html).toMatchSnapshot();
-      done();
+  describe('EC - Media container video', () => {
+    test('Media container video renders correctly', () => {
+      const options = merge(
+        defaultDataStructure,
+        demoContent.sources,
+        demoContent.tracks
+      );
+      expect.assertions(1);
+      return expect(render(options)).resolves.toMatchSnapshot();
     });
   });
 });
