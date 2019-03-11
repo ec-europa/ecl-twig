ebb797ebb15ee80ac40b7c418f84ad8f9b971693
Yannick Huard <mail@yannickhuard.com>
2019-03-11 10:18:19 +0100

    refactor(language-list): improve quality (#68)
    * refactor(language-list): improve quality
    
    * make code more readable
    
    * add spaces

--- src/ec/packages/ec-component-language-list/language-list.test.js
+++ src/ec/packages/ec-component-language-list/language-list.test.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import { renderTwigFileAsNode } from '@ecl-twig/test-utils';
+import { merge, renderTwigFileAsNode } from '@ecl-twig/test-utils';
 
 // Import data for tests
 import { dataSplash, dataOverlay } from './demo/data';
@@ -9,36 +9,80 @@ describe('EC - Language List', () => {
   const render = params => renderTwigFileAsNode(template, params);
 
   describe('Splash', () => {
+    const options = {
+      items: dataSplash.items,
+      overlay: false,
+      logo: {
+        alt: dataSplash.logoAlt,
+        path: '/static/logo--mute.svg',
+      },
+      icon_path: '/static/icons.svg',
+    };
+
     test('renders correctly', () => {
       expect.assertions(1);
+      return expect(render(options)).resolves.toMatchSnapshot();
+    });
 
-      return expect(
-        render({
-          items: dataSplash.items,
-          overlay: false,
-          logo: {
-            alt: dataSplash.logoAlt,
-            path: '/static/logo--mute.svg',
-          },
-          icon_path: '/static/icons.svg',
-        })
-      ).resolves.toMatchSnapshot();
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(options, {
+        extra_classes: 'custom-language-list custom-language-list--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(options, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
     });
   });
 
   describe('Overlay', () => {
+    const options = {
+      items: dataOverlay.items,
+      overlay: true,
+      close_label: dataOverlay.closeLabel,
+      title: dataOverlay.title,
+      icon_path: '/static/icons.svg',
+    };
+
     test('renders correctly', () => {
       expect.assertions(1);
+      return expect(render(options)).resolves.toMatchSnapshot();
+    });
 
-      return expect(
-        render({
-          items: dataOverlay.items,
-          overlay: true,
-          close_label: dataOverlay.closeLabel,
-          title: dataOverlay.title,
-          icon_path: '/static/icons.svg',
-        })
-      ).resolves.toMatchSnapshot();
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(options, {
+        extra_classes: 'custom-language-list custom-language-list--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(options, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
     });
   });
 });
