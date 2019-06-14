add1a93193cd959bdedc30dbe58c10c15ebf4c26
Yannick Huard <mail@yannickhuard.com>
2019-06-14 08:58:03 +0200

    chore: prepare publishing script

---
+++ scripts/publish.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Exit the script on any command with non 0 return code
+set -e
+
+# Echo every command being executed
+set -x
+
+# Go to project root
+cd "$(dirname "$0")"
+cd ..
+
+# Make sure git is clean
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Your git status is not clean. Aborting.";
+  exit 1;
+fi
+
+# Publish updated packages
+./node_modules/.bin/lerna publish from-package "$@"
