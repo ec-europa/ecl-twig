9e6e8a5ec66b93dd4b135275e8cb0510b60e3036
emeryro <emery.romain@gmail.com>
2019-06-20 15:14:17 +0200

    add accordion 2

--- src/ec/packages/ec-component-accordion/accordion.story.js
+++ src/ec/packages/ec-component-accordion/accordion.story.js
@@ -9,6 +9,7 @@ import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import demoData from './demo/data';
 
 import accordion from './accordion.html.twig';
+import accordion2 from './accordion2.html.twig';
 import notes from './docs/accordion.md';
 
 storiesOf('Components/Accordion', module)
@@ -17,6 +18,46 @@ storiesOf('Components/Accordion', module)
   .addDecorator(withNotes)
   .add(
     'default',
+    () => {
+      // This needs to be in the scope of this function.
+      // Called on knob's change of value.
+      demoData.items.forEach(item => {
+        const { id, content, toggle } = item;
+
+        item.toggle.label = text(`Label ${id}`, toggle.label);
+        item.content = text(`Content ${id}`, content);
+
+        item.toggle.icon.path = defaultSprite;
+      });
+
+      const html = accordion2(demoData);
+
+      const demo = document.createDocumentFragment();
+
+      const htmlElement = document.createElement('div');
+      htmlElement.innerHTML = html.trim();
+      demo.appendChild(htmlElement.firstChild);
+
+      const scriptElement = document.createElement('script');
+      scriptElement.innerHTML = `
+        var elements = document.querySelectorAll('[data-ecl-accordion2]');
+
+        for (var i = 0; i < elements.length; i += 1) {
+          var accordion2 = new ECL.Accordion2(elements[i]);
+          accordion2.init();
+        }
+      `;
+      demo.appendChild(scriptElement);
+
+      return demo;
+    },
+
+    {
+      notes: { markdown: notes },
+    }
+  )
+  .add(
+    '[deprecated] ECl < 2.6.0',
     () => {
       // This needs to be in the scope of this function.
       // Called on knob's change of value.
