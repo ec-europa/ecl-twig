d636f6693a038da9fffc43930af4346561d4cbaf
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-09-12 11:25:03 +0200

    Try an alternative way to remove the Webpack ProgressPlugin plugin.

--- src/ec/.storybook/webpack.config.js
+++ src/ec/.storybook/webpack.config.js
@@ -15,14 +15,11 @@ module.exports = ({ config, mode }) => {
 
   // Make it less verbose
   if (mode === 'PRODUCTION') {
-    // Remove ProgressPlugin (4th plugin)
-    const plugin = config.plugins.splice(3, 1);
-
-    if (!(plugin[0] instanceof webpack.ProgressPlugin)) {
-      console.error(
-        'Error: 4th plugin is not ProgressPlugin.\nCheck src/systems/ec/implementations/react/storybook/.storybook/webpack.config.js'
-      );
-      return process.exit(1);
+    for (let i = 0; i < config.plugins.length; i += 1) {
+      if (config.plugins[i] instanceof webpack.ProgressPlugin) {
+        // Remove ProgressPlugin.
+        config.plugins.splice(i, 1);
+      }
     }
   }
 
