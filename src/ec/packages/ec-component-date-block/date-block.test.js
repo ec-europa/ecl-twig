8868721b27481f11f6b71822acf58a97bdea99e7
planctus <davidepuntoferrante@gmail.com>
2019-09-13 15:40:30 +0200

    TWIG-31 - Adding test for extra attributes and time

--- src/ec/packages/ec-component-date-block/date-block.test.js
+++ src/ec/packages/ec-component-date-block/date-block.test.js
@@ -46,4 +46,20 @@ describe('EC - Date Block', () => {
 
     return expect(render(optionsWithTimeSet)).resolves.toMatchSnapshot();
   });
+
+  test('renders correctly with extra attributes and time set', () => {
+    expect.assertions(1);
+
+    const optionsWithExtraClassesAndTime = merge(demoData, {
+      extra_attributes: [
+        { name: 'data-test', value: 'data-test-value' },
+        { name: 'data-test-1', value: 'data-test-value-1' },
+      ],
+      date_time: '15:30',
+    });
+
+    return expect(
+      render(optionsWithExtraClassesAndTime)
+    ).resolves.toMatchSnapshot();
+  });
 });
