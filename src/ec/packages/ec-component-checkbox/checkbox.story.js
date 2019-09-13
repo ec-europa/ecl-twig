0ade1c825e6521e54c6af82f40a6717c38973eb0
Alexis Gaillard <a@bypopcorn.com>
2019-09-13 11:04:18 +0200

    feat(checkbox): add component - TWIG-9

---
+++ src/ec/packages/ec-component-checkbox/checkbox.story.js
@@ -0,0 +1,39 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { withNotes } from '@ecl-twig/storybook-addon-notes';
+import withCode from '@ecl-twig/storybook-addon-code';
+
+import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
+import { dataDefault } from './demo/data';
+
+import checkboxDocs from './README.md';
+import checkboxGroup from './checkbox-group.html.twig';
+
+dataDefault.items.forEach(item => {
+  item.icon_path = defaultSprite; // eslint-disable-line no-param-reassign
+});
+
+storiesOf('Components/Forms/Checkbox', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withNotes)
+  .addDecorator(withCode)
+  .add(
+    'default',
+    () =>
+      checkboxGroup({
+        ...dataDefault,
+        helper_text: text('Help message', dataDefault.helperText),
+        invalid: boolean('Invalid', false),
+        invalid_text: text(
+          'Error message for the group',
+          dataDefault.invalidText
+        ),
+        label: text('Legend', 'Select your preferred destinations'),
+        optional_text: text('Optional text', '(optional)'),
+        required: boolean('Required', false),
+        required_text: text('Required text', '*'),
+      }),
+    {
+      notes: { markdown: checkboxDocs },
+    }
+  );
