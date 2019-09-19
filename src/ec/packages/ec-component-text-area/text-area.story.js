9f65a1e3c3e08d56e191399594792473c2830206
Alexis Gaillard <a@bypopcorn.com>
2019-09-19 14:15:26 +0200

    fix(text-area): update style of select - TWIG-49

--- src/ec/packages/ec-component-text-area/text-area.story.js
+++ src/ec/packages/ec-component-text-area/text-area.story.js
@@ -1,11 +1,18 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
 
+import specData from '@ecl/ec-specs-text-area/demo/data--default';
 import textAreaDocs from './docs/text-area.md';
 
 import textArea from './text-area.html.twig';
+
+const inputWidthOptions = {
+  small: 's',
+  medium: 'm',
+  large: 'l',
+};
 
 storiesOf('Components/Forms/Text area', module)
   .addDecorator(withKnobs)
@@ -13,18 +20,24 @@ storiesOf('Components/Forms/Text area', module)
   .addDecorator(withCode)
   .add(
     'default',
-    () =>
-      textArea({
-        label: text('Label', 'Label'),
-        placeholder: text('Placeholder', 'Placeholder'),
+    () => {
+      const inputWidthSelect = select('Width', inputWidthOptions, 'm');
+
+      return textArea({
+        label: text('Label', specData.label),
+        rows: text('Rows', specData.rows),
+        helper_text: text('Help message', "This is the input's helper text."),
         invalid: boolean('Invalid', false),
-        invalid_text: text('Error message', 'Error message'),
-        helper_text: text('Help message', 'Help message'),
+        invalid_text: text('Invalid text', 'This is the error message'),
         disabled: boolean('Disabled', false),
-        hide_label: boolean('Hide label', false),
+        required: boolean('Required', false),
+        required_text: text('Required Text', '*'),
+        optional_text: text('Optional text', '(optional)'),
+        width: inputWidthSelect,
         id: 'example-id',
         name: 'example-name',
-      }),
+      });
+    },
     {
       notes: { markdown: textAreaDocs },
     }
