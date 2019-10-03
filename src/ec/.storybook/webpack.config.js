ee83fc2ddd4310746b159c694c01312c264c5c2d
planctus <davidepuntoferrante@gmail.com>
2019-10-03 18:00:20 +0200

    feat(twing-loader): progress plugin removal was not acting - TWIG-63

--- src/ec/.storybook/webpack.config.js
+++ src/ec/.storybook/webpack.config.js
@@ -15,11 +15,13 @@ module.exports = ({ config, mode }) => {
 
   // Make it less verbose
   if (mode === 'PRODUCTION') {
-    for (let i = 0; i < config.plugins.length; i += 1) {
-      if (config.plugins[i] instanceof webpack.ProgressPlugin) {
-        // Remove ProgressPlugin.
-        config.plugins.splice(i, 1);
-      }
+    // Remove ProgressPlugin (4th plugin)
+    const plugin = config.plugins.splice(3, 1);
+    if (!(plugin[0].constructor.name === 'ProgressPlugin')) {
+      console.error(
+        'Error: 4th plugin is not ProgressPlugin.\nCheck src/systems/ec/implementations/react/storybook/.storybook/webpack.config.js'
+      );
+      return process.exit(1);
     }
   }
 
