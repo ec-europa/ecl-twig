74bc4c2163b3c3bcb31eccdf641ece624fe339cc
planctus <davidepuntoferrante@gmail.com>
2019-10-09 10:03:41 +0200

    feat(check-release): Changes in the output format - TWIG-67

--- scripts/check-packages.sh
+++ scripts/check-packages.sh
@@ -11,7 +11,7 @@ echo "Hello, you are about to check the release: $mainVersion"
 echo -e "_____________________________________________________________________\n"
 
 # STEP 1 - Check the version of the components to be released against the release version.
-echo "Checking version of the packages:"
+echo "STEP 1: Checking version of the packages"
 errors=0
 
 for version in "${versions[@]}"; do
@@ -25,52 +25,49 @@ if [ "$errors" -gt 0 ]
   then
     if [ "$errors" -gt 1 ]
       then
-        isOrAre='are'
-        package_s='packages'
+        isOrAre="are"
+        package_s="packages"
     else
-      package_s='package'
-      isOrAre='is'
+      package_s="package"
+      isOrAre="is"
     fi
-    echo "There $isOrAre $errors $package_s with mismatched version, please check this, the version you are about to release is $mainVersion."
+    echo ">> There $isOrAre $errors $package_s with mismatched version, please check this, the version you are about to release is $mainVersion."
 else
-  echo "All good about the package versions ;)"
+  echo ">> All good about the package versions ;)"
 fi
 
 echo -e "_____________________________________________________________________\n"
 
 # STEP 2 - First check the number of packages:
-echo "Checking the release packages:"
+echo "STEP 2: Checking the release packages"
 numOfPackages="$($jq -r '[(.dependencies) | keys] | flatten | length' $packageJson)"
-echo "You have $numOfPackages components in your list and"
+echo -n ">> You have $numOfPackages components in your list and "
 numOfFolders="$(cd ./src/ec/packages/ && echo ec-component-*/ | wc -w)"
-echo "you have $numOfFolders packages folders in your filesystem.";
+echo "$numOfFolders packages folders in your filesystem.";
 
 if [ $numOfFolders == $numOfPackages ]
   then
-    echo "All good for now, then..."
+    echo "All good for now, then."
 elif [ $(($numOfFolders - $numOfPackages)) -lt 0 ]
   then
     echo "It seems you are about to release more components than the ones we have."
 elif [ $(($numOfFolders - $numOfPackages)) -gt 0 ]
   then
-    echo "Ops, why do you have $((numOfFolders - numOfPackages)) folder(s) more..?"
+    echo -n "Ops, why do you have $((numOfFolders - numOfPackages)) folder(s) more? "
     echo "Looks like you might be forgetting something to release..."
 fi
 
 echo -e "_____________________________________________________________________\n"
 
 # STEP 3 - Check the packages against the folders.
-echo "Checking the packages list against the folders you have:"
+echo "STEP 3: Checking the packages list against the folders you have"
 packages=($(<$packageJson $jq -r '.dependencies | keys | .[] | sub("@ecl-twig/"; ""; "g") | @sh'))
 packagesFolders=(./src/ec/packages/ec-component-*)
 foldersArray=()
-
+# jq returns the items with '', we mimic that to not
+# have to further process the data.
 for package in "${packagesFolders[@]}"; do
   package=${package##*/}
-  if [ $package == "ec-components" ]
-    then
-      continue
-  fi
   foldersArray+=("'$package'")
 done
 
@@ -80,10 +77,10 @@ B=${packages[@]}
 
 if [ "$A" == "$B" ]
   then
-    echo "Yes, it seems that you're all set!!! Nice ;)"
+    echo ">> Yes, it seems that you're all set!!! Nice ;)"
 else
-  echo "We found issues, trying to guess what they are about..."
-  # We try to find the mismatches.
+  echo ">> We found issues, trying to guess what they are about."
+  # We try to find the mismatches between the two arrays.
   packagesDiff=()
   for i in "${foldersArray[@]}"; do
     skip=
