593cc18ee9e03836ac95dc1a31ba21cdd50f562c
planctus <davidepuntoferrante@gmail.com>
2019-10-15 17:44:22 +0200

    feat(site-header-core): adding component files - TWIG-79

---
+++ src/ec/packages/ec-component-site-header-core/demo/data.js
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-extraneous-dependencies, no-param-reassign  */
+import specEnglishData from '@ecl/ec-specs-site-header/demo/data--en';
+import specFrenchData from '@ecl/ec-specs-site-header/demo/data--fr';
+
+const defaultSprite = 'static/icons.svg';
+
+const englishBanner = 'static/media/logo--en.svg';
+const frenchBanner = 'static/media/logo--fr.svg';
+
+const adapter = initialData => {
+  const adaptedData = JSON.parse(JSON.stringify(initialData));
+
+  const lng = adaptedData.logo.language;
+  adaptedData.logo.src = lng === 'en' ? englishBanner : frenchBanner;
+
+  adaptedData.language_selector = adaptedData.languageSelector;
+  delete adaptedData.languageSelector;
+
+  adaptedData.language_selector.overlay.close_label =
+    adaptedData.language_selector.overlay.closeLabel;
+  delete adaptedData.language_selector.overlay.closeLabel;
+
+  adaptedData.language_selector.overlay.items.forEach(item => {
+    item.path = item.href;
+    delete item.href;
+    if (item.isActive) {
+      item.active = true; // eslint-disable-line no-param-reassign
+      delete item.isActive;
+    }
+  });
+
+  adaptedData.search_form = {
+    text_input: {
+      id: adaptedData.searchForm.textInputId,
+      name: adaptedData.searchForm.inputLabel,
+      label: adaptedData.searchForm.inputLabel,
+    },
+    button: {
+      label: adaptedData.searchForm.buttonLabel,
+    },
+  };
+  delete adaptedData.searchForm;
+
+  adaptedData.icon_file_path = defaultSprite;
+
+  return adaptedData;
+};
+
+export const englishData = adapter(specEnglishData);
+export const frenchData = adapter(specFrenchData);
---
+++ src/ec/packages/ec-component-site-header-core/site-header-core.story.js
@@ -0,0 +1,40 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withNotes } from '@ecl-twig/storybook-addon-notes';
+import withCode from '@ecl-twig/storybook-addon-code';
+
+import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
+import englishBanner from '@ecl/ec-resources-logo/logo--en.svg';
+import frenchBanner from '@ecl/ec-resources-logo/logo--fr.svg';
+
+import { englishData, frenchData } from './demo/data';
+
+import siteHeaderDocs from './README.md';
+import siteHeader from './site-header-core.html.twig';
+
+storiesOf('Components/Site Headers/Core', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withNotes)
+  .addDecorator(withCode)
+  .add(
+    'default',
+    () => {
+      englishData.icon_file_path = defaultSprite;
+      englishData.logo.src = englishBanner;
+      return siteHeader(englishData);
+    },
+    {
+      notes: { markdown: siteHeaderDocs },
+    }
+  )
+  .add(
+    'translated',
+    () => {
+      frenchData.icon_file_path = defaultSprite;
+      frenchData.logo.src = frenchBanner;
+      return siteHeader(frenchData);
+    },
+    {
+      notes: { markdown: siteHeaderDocs },
+    }
+  );
