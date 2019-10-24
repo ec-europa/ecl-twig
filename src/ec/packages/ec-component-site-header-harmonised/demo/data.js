79dc69dc49ac963d8b56df7d8f93eca8b690882f
planctus <davidepuntoferrante@gmail.com>
2019-10-24 10:55:46 +0200

    feat(site-header-harmonised): Adding package - TWIG-80

---
+++ src/ec/packages/ec-component-site-header-harmonised/demo/data.js
@@ -0,0 +1,68 @@
+/* eslint-disable import/no-extraneous-dependencies, no-param-reassign */
+import specGroup1 from '@ecl/ec-specs-site-header-harmonised/demo/data--group1';
+import specGroup2 from '@ecl/ec-specs-site-header-harmonised/demo/data--group2';
+
+const defaultSprite = 'static/icons.svg';
+
+const logo = 'static/media/logo--en.svg';
+
+const adapter = initialData => {
+  const adaptedData = JSON.parse(JSON.stringify(initialData));
+  adaptedData.banner_top = adaptedData.bannerTop;
+  delete adaptedData.bannerTop;
+  // Login toggle.
+  if (adaptedData.loginToggle) {
+    adaptedData.login_toggle = {
+      label_not_logged: adaptedData.loginToggle.labelNotLogged,
+      href_not_logged: adaptedData.loginToggle.hrefNotLogged,
+      label_logged: adaptedData.loginToggle.labelLogged,
+      href_logged: adaptedData.loginToggle.hrefLogged,
+    };
+    delete adaptedData.loginToggle;
+  }
+  // Login box.
+  adaptedData.login_box = adaptedData.loginBox;
+  delete adaptedData.loginBox;
+  // Language selector.
+  adaptedData.logo.src = logo;
+
+  adaptedData.language_selector = adaptedData.languageSelector;
+  delete adaptedData.languageSelector;
+
+  adaptedData.language_selector.overlay.close_label =
+    adaptedData.language_selector.overlay.closeLabel;
+  delete adaptedData.language_selector.overlay.closeLabel;
+
+  adaptedData.language_selector.overlay.items.forEach(item => {
+    item.path = item.href;
+    delete item.href;
+    if (item.isActive) {
+      item.active = true; // eslint-disable-line no-param-reassign
+      delete item.isActive;
+    }
+  });
+  // Search toggle.
+  adaptedData.search_toggle = adaptedData.searchToggle;
+  // Search form.
+  adaptedData.search_form = {
+    extra_attributes: [{ name: 'id', value: adaptedData.searchForm.id }],
+    text_input: {
+      id: adaptedData.searchForm.textInputId,
+      name: adaptedData.searchForm.inputLabel,
+      label: adaptedData.searchForm.inputLabel,
+    },
+    button: {
+      label: adaptedData.searchForm.buttonLabel,
+    },
+  };
+  delete adaptedData.searchForm;
+
+  adaptedData.search_toggle = adaptedData.searchToggle;
+  delete adaptedData.searchToggle;
+
+  adaptedData.icon_file_path = defaultSprite;
+  return adaptedData;
+};
+
+export const dataGroup1 = adapter(specGroup1);
+export const dataGroup2 = adapter(specGroup2);
---
+++ src/ec/packages/ec-component-site-header-harmonised/site-header-harmonised.test.js
@@ -0,0 +1,69 @@
+import { merge, renderTwigFileAsNode } from '@ecl-twig/test-utils';
+
+import { dataGroup1, dataGroup2 } from './demo/data';
+
+describe('EC - Site Header Core', () => {
+  const template =
+    '@ecl-twig/ec-component-site-header-harmonised/site-header-harmonised.html.twig';
+  const render = params => renderTwigFileAsNode(template, params);
+
+  describe('Group 1', () => {
+    test('renders correctly', () => {
+      expect.assertions(1);
+      return expect(render(dataGroup1)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(dataGroup1, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(dataGroup1, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+  });
+
+  describe('Group 2', () => {
+    test('renders correctly', () => {
+      expect.assertions(1);
+      return expect(render(dataGroup2)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra class names', () => {
+      expect.assertions(1);
+
+      const withExtraClasses = merge(dataGroup2, {
+        extra_classes: 'custom-class custom-class--test',
+      });
+
+      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+    });
+
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
+
+      const withExtraAttributes = merge(dataGroup2, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+    });
+  });
+});
