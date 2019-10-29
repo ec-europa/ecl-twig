8a3e22575efa7dc1b51cb47dda647129eefbda75
planctus <davidepuntoferrante@gmail.com>
2019-10-29 15:54:04 +0100

    feat(breadcrumb-core): Add component - TWIG-97

---
+++ src/ec/packages/ec-component-breadcrumb-core/demo/data.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import specDataSimple from '@ecl/ec-specs-breadcrumb-core/demo/data-simple';
+import specDataLong from '@ecl/ec-specs-breadcrumb-core/demo/data';
+import { formatLinkAlt } from '@ecl-twig/data-utils';
+
+const adapter = initialData => {
+  // Copy reference specification demo data.
+  const adaptedData = JSON.parse(JSON.stringify(initialData));
+  adaptedData.links = adaptedData.items.map(formatLinkAlt);
+  delete adaptedData.items;
+  return adaptedData;
+};
+
+export const dataSimple = adapter(specDataSimple);
+export const dataLong = adapter(specDataLong);
