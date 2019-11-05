c6ce6cf3503c9e968979e8442a9618788d9c6442
planctus <davidepuntoferrante@gmail.com>
2019-11-05 17:33:21 +0100

    feat(page-header-core): including breadcrumb core - TWIG-101

---
+++ src/ec/packages/ec-component-page-header-core/demo/data.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import demoTitle from '@ecl/ec-specs-page-header-core/demo/data--title';
+import demoMetaTitle from '@ecl/ec-specs-page-header-core/demo/data--meta-title';
+import demoMetaTitleDescription from '@ecl/ec-specs-page-header-core/demo/data--meta-title-description';
+import { dataLong } from '../../ec-component-breadcrumb-core/demo/data';
+
+const adapter = initialData => {
+  // Copy reference specification demo data.
+  const adaptedData = JSON.parse(JSON.stringify(initialData));
+  adaptedData.breadcrumb = dataLong;
+  return adaptedData;
+};
+
+export const demoTitleContent = adapter(demoTitle);
+export const demoMetaTitleContent = adapter(demoMetaTitle);
+export const demoMetaTitleDescriptionContent = adapter(
+  demoMetaTitleDescription
+);
