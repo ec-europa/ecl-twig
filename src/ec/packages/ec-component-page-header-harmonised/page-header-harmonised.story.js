0ef284775626ff83c05ee84985fe37f9ec459cb2
planctus <davidepuntoferrante@gmail.com>
2019-11-06 10:11:11 +0100

    feat(page-header-harmonised): Aligning the component - TWIG-102

--- src/ec/packages/ec-component-page-header-harmonised/page-header-harmonised.story.js
+++ src/ec/packages/ec-component-page-header-harmonised/page-header-harmonised.story.js
@@ -1,108 +1,47 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
 
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
+import {
+  demoTitleContent,
+  demoMetaTitleContent,
+  demoMetaTitleDescriptionContent,
+} from './demo/data';
+import pageHeaderStandardisedDocs from './README.md';
+import pageHeaderStandardised from './page-header-harmonised.html.twig';
 
-import breadcrumbDataSimple from '@ecl/ec-specs-breadcrumb-core/demo/data-simple';
-
-import pageHeaderHarmonisedDataTitle from '@ecl/ec-specs-page-header-harmonised/demo/data--title';
-import pageHeaderHarmonisedDataMetaTitle from '@ecl/ec-specs-page-header-harmonised/demo/data--meta-title';
-import pageHeaderHarmonisedDataMetaTitleDescription from '@ecl/ec-specs-page-header-harmonised/demo/data--meta-title-description';
-
-import pageHeaderHarmonisedDocs from './README.md';
-import pageHeaderHarmonised from './page-header-harmonised.html.twig';
-
-function formatBreadcrumbLink(l) {
-  const link = {
-    label: l.label,
-    path: l.href,
-  };
-
-  return link;
-}
-
-function formatPageHeaderHarmonisedInfo(i, index) {
-  const iconType = i.icon.split('--');
-  const info = {
-    text: text(`Info ${index} text`, i.text),
-    icon: {
-      type: iconType[0],
-      name: iconType[1],
-      path: defaultSprite,
-    },
-  };
-
-  return info;
-}
-
-function preparePageHeaderHarmonisedData(data) {
-  const output = {};
-
-  output.breadcrumb = {
-    links: breadcrumbDataSimple.items.map(formatBreadcrumbLink),
-    navigation_text: breadcrumbDataSimple.label,
-    icon_file_path: defaultSprite,
-  };
-
-  output.title = text('Title', data.title);
-
-  if (data.description) {
-    output.description = text('Description', data.description);
-  }
-
-  if (data.meta) {
-    output.meta = text('Meta', data.meta);
-  }
-
-  if (data.infos) {
-    output.infos = data.infos.map(formatPageHeaderHarmonisedInfo);
-  }
-
-  return output;
-}
+demoTitleContent.breadcrumb.icon_file_path = defaultSprite;
+demoMetaTitleContent.breadcrumb.icon_file_path = defaultSprite;
+demoMetaTitleDescriptionContent.breadcrumb.icon_file_path = defaultSprite;
 
 storiesOf('Components/Page Headers/Page Header Harmonised', module)
-  .addDecorator(withKnobs)
   .addDecorator(withNotes)
   .addDecorator(withCode)
   .add(
     'title',
     () => {
-      const sampleData = preparePageHeaderHarmonisedData(
-        pageHeaderHarmonisedDataTitle
-      );
-
-      return pageHeaderHarmonised(sampleData);
+      return pageHeaderStandardised(demoTitleContent);
     },
     {
-      notes: { markdown: pageHeaderHarmonisedDocs },
+      notes: { markdown: pageHeaderStandardisedDocs },
     }
   )
   .add(
     'meta-title',
     () => {
-      const sampleData = preparePageHeaderHarmonisedData(
-        pageHeaderHarmonisedDataMetaTitle
-      );
-
-      return pageHeaderHarmonised(sampleData);
+      return pageHeaderStandardised(demoMetaTitleContent);
     },
     {
-      notes: { markdown: pageHeaderHarmonisedDocs },
+      notes: { markdown: pageHeaderStandardisedDocs },
     }
   )
   .add(
     'meta-title-description',
     () => {
-      const sampleData = preparePageHeaderHarmonisedData(
-        pageHeaderHarmonisedDataMetaTitleDescription
-      );
-
-      return pageHeaderHarmonised(sampleData);
+      return pageHeaderStandardised(demoMetaTitleDescriptionContent);
     },
     {
-      notes: { markdown: pageHeaderHarmonisedDocs },
+      notes: { markdown: pageHeaderStandardisedDocs },
     }
   );
--- src/ec/packages/ec-component-page-header-harmonised/page-header-harmonised.test.js
+++ src/ec/packages/ec-component-page-header-harmonised/page-header-harmonised.test.js
@@ -1,69 +1,22 @@
 import { merge, renderTwigFileAsNode } from '@ecl-twig/test-utils';
 
-import breadcrumbDataSimple from '@ecl/ec-specs-breadcrumb-core/demo/data-simple';
+import { dataSimple, dataLong } from './demo/data';
 
-import pageHeaderHarmonisedDataTitle from '@ecl/ec-specs-page-header-harmonised/demo/data--title';
-import pageHeaderHarmonisedDataMetaTitle from '@ecl/ec-specs-page-header-harmonised/demo/data--meta-title';
-import pageHeaderHarmonisedDataMetaTitleDescription from '@ecl/ec-specs-page-header-harmonised/demo/data--meta-title-description';
-
-function formatBreadcrumbLink(l) {
-  const link = {
-    label: l.label,
-    path: l.href,
-  };
-
-  return link;
-}
-
-function formatPageHeaderHarmonisedInfo(i) {
-  const iconType = i.icon.split('--');
-  const info = {
-    text: i.text,
-    icon: {
-      type: iconType[0],
-      name: iconType[1],
-      path: 'static/icons.svg',
-    },
-  };
-
-  return info;
-}
-
-function preparePageHeaderHarmonisedData(data) {
-  const output = {};
-
-  output.breadcrumb = {
-    links: breadcrumbDataSimple.items.map(formatBreadcrumbLink),
-    navigation_text: breadcrumbDataSimple.label,
-    icon_file_path: 'static/icons.svg',
-  };
-
-  output.title = data.title;
-
-  if (data.description) {
-    output.description = data.description;
-  }
-
-  if (data.meta) {
-    output.meta = data.meta;
-  }
-
-  if (data.infos) {
-    output.infos = data.infos.map(formatPageHeaderHarmonisedInfo);
-  }
-
-  return output;
-}
-
-describe('EC - Page Header Harmonised', () => {
+describe('EC - Breadcrumb Harmonised', () => {
   const template =
-    '@ecl-twig/ec-component-page-header-harmonised/page-header-harmonised.html.twig';
+    '@ecl-twig/ec-component-breadcrumb-harmonised/breadcrumb-harmonised.html.twig';
   const render = params => renderTwigFileAsNode(template, params);
+  const defaultIconPath = 'static/icons.svg';
 
-  describe('title', () => {
-    const data = preparePageHeaderHarmonisedData(pageHeaderHarmonisedDataTitle);
+  describe('Simple', () => {
+    const data = {
+      links: dataSimple.links,
+      icon_file_path: defaultIconPath,
+      navigation_text: dataSimple.navigation_text,
+      ellipsis_label: 'Click to expand',
+    };
 
-    test(`- renders correctly`, () => {
+    test(`renders correctly`, () => {
       expect.assertions(1);
       return expect(render(data)).resolves.toMatchSnapshot();
     });
@@ -71,33 +24,39 @@ describe('EC - Page Header Harmonised', () => {
     test('renders correctly with extra class names', () => {
       expect.assertions(1);
 
-      const withExtraClasses = merge(data, {
+      const optionsWithExtraClasses = merge(data, {
         extra_classes: 'custom-class custom-class--test',
       });
 
-      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+      return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
     });
 
     test('renders correctly with extra attributes', () => {
       expect.assertions(1);
 
-      const withExtraAttributes = merge(data, {
+      const optionsWithExtraClasses = merge(data, {
         extra_attributes: [
           { name: 'data-test', value: 'data-test-value' },
           { name: 'data-test-1', value: 'data-test-value-1' },
         ],
       });
 
-      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+      return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
     });
   });
 
-  describe('meta-title', () => {
-    const data = preparePageHeaderHarmonisedData(
-      pageHeaderHarmonisedDataMetaTitle
-    );
+  describe('Long', () => {
+    const data = {
+      extra_attributes: [
+        { name: 'data-ecl-auto-init', value: 'BreadcrumbStandardised' },
+      ],
+      links: dataLong.links,
+      icon_file_path: defaultIconPath,
+      navigation_text: dataLong.navigation_text,
+      ellipsis_label: 'Click to expand',
+    };
 
-    test(`- renders correctly`, () => {
+    test(`renders correctly`, () => {
       expect.assertions(1);
       return expect(render(data)).resolves.toMatchSnapshot();
     });
@@ -105,58 +64,24 @@ describe('EC - Page Header Harmonised', () => {
     test('renders correctly with extra class names', () => {
       expect.assertions(1);
 
-      const withExtraClasses = merge(data, {
+      const optionsWithExtraClasses = merge(data, {
         extra_classes: 'custom-class custom-class--test',
       });
 
-      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
+      return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
     });
 
     test('renders correctly with extra attributes', () => {
       expect.assertions(1);
 
-      const withExtraAttributes = merge(data, {
+      const optionsWithExtraClasses = merge(data, {
         extra_attributes: [
           { name: 'data-test', value: 'data-test-value' },
           { name: 'data-test-1', value: 'data-test-value-1' },
         ],
       });
 
-      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
-    });
-  });
-
-  describe('meta-title-description', () => {
-    const data = preparePageHeaderHarmonisedData(
-      pageHeaderHarmonisedDataMetaTitleDescription
-    );
-
-    test(`- renders correctly`, () => {
-      expect.assertions(1);
-      return expect(render(data)).resolves.toMatchSnapshot();
-    });
-
-    test('renders correctly with extra class names', () => {
-      expect.assertions(1);
-
-      const withExtraClasses = merge(data, {
-        extra_classes: 'custom-class custom-class--test',
-      });
-
-      return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
-    });
-
-    test('renders correctly with extra attributes', () => {
-      expect.assertions(1);
-
-      const withExtraAttributes = merge(data, {
-        extra_attributes: [
-          { name: 'data-test', value: 'data-test-value' },
-          { name: 'data-test-1', value: 'data-test-value-1' },
-        ],
-      });
-
-      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
+      return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
     });
   });
 });
