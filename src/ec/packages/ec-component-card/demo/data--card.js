7f469f379c95d55e2cef05c4468bf839fd6f7876
planctus <davidepuntoferrante@gmail.com>
2019-11-18 16:36:32 +0100

    feat(card): moving adapter.js outside of the specs folder - TWIG-132

--- src/ec/packages/ec-component-card/demo/data--card.js
+++ src/ec/packages/ec-component-card/demo/data--card.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies, unicorn/filename-case */
 import specCard from '@ecl/ec-specs-card/demo/data--card';
-import adapter from './adapter';
+import adapter from '../adapter';
 
 export default adapter(specCard);
--- src/ec/packages/ec-component-card/demo/data--tile.js
+++ src/ec/packages/ec-component-card/demo/data--tile.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, unicorn/filename-case */
 import specTile from '@ecl/ec-specs-card/demo/data--tile';
-import adapter from './adapter';
+import adapter from '../adapter';
 
 specTile.type = 'tile';
 
