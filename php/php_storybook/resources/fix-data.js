a1f3e3aa77d31dff66f0797da2eee9937dc4c2ce
planctus <davidepuntoferrante@gmail.com>
2019-11-18 10:02:46 +0100

    feat(php-storybook): fixinng fix-data.js

--- php/php_storybook/resources/fix-data.js
+++ php/php_storybook/resources/fix-data.js
@@ -40,33 +40,33 @@ function fixDataInput(data, componentRootName) {
     data.card = {};
     data.card.description = data.description;
     data.card.title = data.title;
-    delete(data.description);
+    delete data.description;
     if (data.meta) {
       const meta = data.meta;
       data.card.meta = [];
       data.card.meta.push(meta);
-      delete(data.meta);
+      delete data.meta;
     }
     if (data.tags) {
       data.card.tags = data.tags;
-      delete(data.tags);
+      delete data.tags;
     }
     if (data.card.title.href) {
       data.card.title.path = data.card.title.href;
     }
     if (data.image) {
       data.card.image = data.image;
-      delete(data.image);
+      delete data.image;
     }
     if (data.infos) {
       data.card.infos = data.infos;
-      delete(data.infos);
+      delete data.infos;
     }
     if (data.links) {
       data.card.links = data.links;
-      delete(data.links);
+      delete data.links;
     }
-    delete(data.title);
+    delete data.title;
   } else if (componentRootName === 'link') {
     data.link = {
       type: data.variant,
@@ -77,12 +77,12 @@ function fixDataInput(data, componentRootName) {
       data.link.icon_position = 'after';
       data.icon.path = '/icons.svg';
       data.icon.name = data.icon.shape;
-      delete(data.icon.shape);
+      delete data.icon.shape;
     }
-    delete(data.variant);
-    delete(data.label);
-    delete(data.href);
-    delete(data.title);
+    delete data.variant;
+    delete data.label;
+    delete data.href;
+    delete data.title;
   } else if (componentRootName === 'icon') {
     if (data.shape) {
       data.icon = {};
