206689957545acbe143149eafa152a8ecb64b55c
planctus <davidepuntoferrante@gmail.com>
2019-11-27 17:33:20 +0100

    feat(php-storybook): redcing amount of data fixes thanks to the improvements in the library

--- php/php_storybook/resources/fix-data.js
+++ php/php_storybook/resources/fix-data.js
@@ -10,39 +10,9 @@ function fixDataInput(data, componentRootName) {
         path: '/example',
       },
     };
-  } else if (componentRootName === 'language-list') {
-    data = data.dataSplash;
-    data.icon_file_path = '/icons.svg';
-    data.logo = {};
-    data.logo.path = '/logo--mute.svg';
-    data.logo.alt = 'European Commission logo';
-  } else if (componentRootName === 'link') {
-    data.link = {
-      type: data.variant,
-      label: data.label,
-      path: data.href,
-    };
-    if (data.icon) {
-      data.link.icon_position = 'after';
-      data.icon.path = '/icons.svg';
-      data.icon.name = data.icon.shape;
-      delete data.icon.shape;
-    }
-    delete data.variant;
-    delete data.label;
-    delete data.href;
-    delete data.title;
-  } else if (componentRootName === 'icon') {
-    if (data.shape) {
-      data.icon = {};
-      data.icon.name = data.shape;
-    }
   } else if (componentRootName === 'file') {
     data = data.dataWithTranslation;
-  } else if (
-    componentRootName === 'text-area' ||
-    componentRootName === 'text-input'
-  ) {
+  } else if (componentRootName === 'text-area') {
     if (data.has_error) {
       data.invalid = true;
       data.invalid_text = 'This is the error message';
@@ -54,14 +24,6 @@ function fixDataInput(data, componentRootName) {
     data.optional_text = '(optional)';
     data.name = 'example-name';
     data.label = 'Label';
-  } else if (data.dataDefault) {
-    data = data.dataDefault;
-    if (componentRootName === 'hero-banner') {
-      if (data.link) {
-        data.link = [];
-        data.link.link = data.link;
-      }
-    }
   } else if (data.bannerDataDefault) {
     data = data.bannerDataDefault;
   } else if (data.dataInfo) {
