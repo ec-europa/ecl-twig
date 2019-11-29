7e83b412fe8e0c7ef7616df0c02b40ee98d454b9
Alexis Gaillard <a@bypopcorn.com>
2019-11-29 18:13:10 +0100

    refactor(site-header-standardised): correct english specs import path - TWIG-151

--- src/ec/packages/ec-component-site-header-standardised/demo/data--en.js
+++ src/ec/packages/ec-component-site-header-standardised/demo/data--en.js
@@ -1,4 +1,4 @@
-import specs from '@ecl/ec-specs-site-header/demo/data--en';
+import specs from '@ecl/ec-specs-site-header-standardised/demo/data--en';
 import adapter from '../adapter';
 
 export default adapter(specs);
