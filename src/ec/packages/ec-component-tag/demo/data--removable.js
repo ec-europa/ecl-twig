40c845a46199837a99e6407b4b0cfbfcc58518dd
planctus <davidepuntoferrante@gmail.com>
2019-11-29 15:33:07 +0100

    improvement(tag): specs file, adapting test and story - TWIG-124

---
+++ src/ec/packages/ec-component-tag/demo/data--removable.js
@@ -0,0 +1,10 @@
+const data = {
+  tag: {
+    label: 'Removable tag label',
+    type: 'removable',
+    aria_label: 'Dismiss',
+  },
+  default_icon_path: '/icons.svg',
+};
+
+export default data;
