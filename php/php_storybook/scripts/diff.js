ef69039694c5fb6c4b698be3308f993cd915c2ed
planctus <davidepuntoferrante@gmail.com>
2019-12-09 21:00:44 +0100

    feat(diff): diff.js improvements

--- php/php_storybook/scripts/diff.js
+++ php/php_storybook/scripts/diff.js
@@ -7,8 +7,6 @@ const { HtmlDiffer } = require('html-differ');
 const logger = require('html-differ/lib/logger');
 
 const system = 'ec';
-const jsExtension = '.js.html';
-const phpExtension = '.php.html';
 const singleComponent = process.argv.slice(2);
 const rootFolder = process.cwd();
 const distFolder = `${rootFolder}/php`;
@@ -35,23 +33,27 @@ const failed = [];
 components.forEach(component => {
   totalComponents += 1;
   console.log(`\nChecking component: ${component}:`);
-  console.log(`----------------------------------`);
+  console.log(`-------------------------------------------------------`);
   const dataFiles = fs.readdirSync(`${systemFolder}/${component}/specs`);
+  const markup = [];
+  const filenames = [];
+
   dataFiles.forEach(dataFile => {
     totalVariants += 1;
-    const jsFileName =
-      dataFile.replace('data', component).slice(0, -5) + jsExtension;
-    const phpFileName =
-      dataFile.replace('data', component).slice(0, -5) + phpExtension;
-    const jsMarkup = fs
-      .readFileSync(`${systemFolder}/${component}/js/${jsFileName}`, 'utf-8')
-      .toString();
-    const phpMarkup = fs
-      .readFileSync(`${systemFolder}/${component}/${phpFileName}`, 'utf-8')
-      .toString();
 
-    const diff = htmlDiffer.diffHtml(jsMarkup, phpMarkup);
-    const isEqual = htmlDiffer.isEqual(jsMarkup, phpMarkup);
+    ['js', 'php'].forEach(lang => {
+      const extension = `.${lang}.html`;
+      const js = lang === 'js' ? lang : '';
+      const fileName =
+        dataFile.replace('data', component).slice(0, -5) + extension;
+      filenames.push(fileName);
+      markup.lang = fs
+        .readFileSync(`${systemFolder}/${component}/${js}/${fileName}`, 'utf-8')
+        .toString();
+    });
+
+    const diff = htmlDiffer.diffHtml(markup.js, markup.php);
+    const isEqual = htmlDiffer.isEqual(markup.js, markup.php);
     let res = '';
 
     if (isEqual) {
@@ -61,21 +63,20 @@ components.forEach(component => {
       res = logger.logDiffText(diff, { charsAroundDiff: 40 });
       failed.push(component);
     }
-    console.log(`Comparing ${jsFileName} with ${phpFileName}:`);
+    console.log(`Comparing ${filenames.join(' / ')}: ${res}`);
     console.log(`${res}`);
   });
 });
 
-let percent = (100 * matches) / totalVariants;
-percent = percent.toFixed(2);
+const percent = (100 * matches) / totalVariants;
+percent.toFixed(2);
 
 console.log(
   `\nWe've been comparing ${totalVariants} variants in ${totalComponents} components.`
 );
 console.log(`With ${matches} perfect* matches, the ${percent}%.`);
-
 if (failed.length > 0) {
-  console.log(`You might want to check: ${failed.toString()}`);
+  console.log(`You might want to check: ${failed.join()}`);
 }
 console.log(`\n* The files we check are formatted with https://www.npmjs.com/package/prettier,
   for the diff we use https://www.npmjs.com/package/html-differ, with this conf:`);
