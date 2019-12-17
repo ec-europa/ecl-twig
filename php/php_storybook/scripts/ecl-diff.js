9b1437a26bb957303a4c1d7f21f864f19de51c72
planctus <davidepuntoferrante@gmail.com>
2019-12-17 15:11:43 +0100

    feat(ecl-diff): Offering lists when possible - TWIG-180

--- php/php_storybook/scripts/ecl-diff.js
+++ php/php_storybook/scripts/ecl-diff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-/* eslint-disable import/no-extraneous-dependencies, no-console, no-param-reassign, no-restricted-syntax, no-await-in-loop */
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, no-console,
+no-param-reassign, no-restricted-syntax, no-await-in-loop */
 
 const fs = require('fs');
 const htmlDiffer = require('html-differ');
@@ -8,33 +9,59 @@ const logger = require('html-differ/lib/logger');
 const puppeteer = require('puppeteer');
 const decode = require('decode-html');
 const yargsInteractive = require('yargs-interactive');
-
-const options = {
-  interactive: { default: true },
-  component: {
-    type: 'input',
-    default: '',
-    describe: 'The name of the base component (with no variant)',
-  },
-  variant: {
-    type: 'input',
-    default: '',
-    describe: 'The name of the twig variant (what is after the --) ',
-  },
-  eclStory: {
-    type: 'input',
-    default: '',
-    describe:
-      'The name of the story in the ECL storybook (what is after the --)',
-  },
-};
+// Retrieve the list of packages from our fs.
+let packages = require('../../../../src/ec/.storybook/ec-packages.js').list;
 
 const system = 'ec';
 const rootFolder = process.cwd();
 const distFolder = `${rootFolder}/php`;
 const systemFolder = `${distFolder}/packages/${system}`;
-const eclPath =
-  'https://ec.europa.eu/component-library/playground/ec/?path=/story/components-';
+
+const getBase = element => {
+  [, element] = element.split('ec-component-');
+  return element;
+};
+// We build a list of components by their root name.
+packages = packages.map(getBase);
+packages.pop();
+
+// The questions we need an answer for.
+const options = {
+  interactive: { default: true },
+  component: {
+    type: 'list',
+    choices: packages,
+    describe: 'The name of the base component',
+  },
+  eclSection: {
+    type: 'list',
+    describe: 'The group the component is associated with in ECL',
+    choices: [
+      'components',
+      'page-structure',
+      'components-forms',
+      'components-navigation',
+      'components-banners',
+    ],
+  },
+  variant: {
+    type: 'input',
+    default: '',
+    describe: 'The name of the twig variant (what is after the -- Ex: audio) ',
+  },
+  eclStory: {
+    type: 'input',
+    default: 'default',
+    prompt: 'always',
+    describe:
+      'The name of the story in the ECL storybook (what is after the -- Ex: default)',
+  },
+  confirm: {
+    type: 'confirm',
+    describe: `Do you confirm the choices you've made?`,
+  },
+};
+
 const extension = `.php.html`;
 const diffOptions = {
   ignoreAttributes: false,
@@ -51,56 +78,26 @@ const getVariant = element => {
 };
 
 yargsInteractive()
+  .usage('$0 <command> [args]')
   .interactive(options)
   .then(result => {
     (async () => {
       try {
-        let res = '';
-        const { component } = result;
-        const { eclStory } = result;
-        const { variant } = result;
+        const { component, eclStory, variant, eclSection, confirm } = result;
+
+        if (!confirm) {
+          console.error('Please run again yarn ecl-diff, then.');
+          process.exit(1);
+        }
 
         if (!component) {
           console.error(
             `We need to know which component and variant you want to test`
           );
+          process.exit(1);
         }
-
-        const eclFinalUrl = `${eclPath + component}--${eclStory}`;
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(eclFinalUrl);
-        await page.click(`div#components-${component}`);
-        const stories = await page.$$(
-          `a#explorercomponents-${component} + div a`
-        );
-        if (stories) {
-          let hrefs = [];
-          for (const story of stories) {
-            const href = await page.evaluate(
-              el => el.getAttribute('href'),
-              story
-            );
-            if (eclFinalUrl === `https://ec.europa.eu${href}`) {
-              hrefs = false;
-              break;
-            }
-
-            const eclVariant = href.split('--')[1];
-            hrefs.push(eclVariant);
-          }
-
-          if (hrefs.length > 0) {
-            console.error(
-              `We couldn't find the variant ${eclStory} in the ECL website, but we've found these: ${hrefs.join(
-                ', '
-              )}`
-            );
-          }
-        }
-
-        await page.click('button[title="Show HTML"]');
-
+        // We try to use the variant as it comes from the user, if we don't find it
+        // we return the available variants in ecl-twig.
         let fileName = '';
         if (variant) {
           fileName = `${component}--${variant}${extension}`;
@@ -127,12 +124,59 @@ yargsInteractive()
           phpFiles = phpFiles.map(getVariant);
 
           console.error(
-            `It seems that "${variant}" has not been found, but we've found these: ${phpFiles.join(
+            `The "${variant}" variant for the "${component}" component has not been found in ecl-twig, but we've found these alternatives: ${phpFiles.join(
               ', '
             )}`
           );
           process.exit(1);
         }
+        // Now we process the story in ECL,we try to retrieve all the stories available
+        // and see if any of them matches the requested one, if none does we return the
+        // list of stories available for a component, if we found them.
+        const eclPath = `https://ec.europa.eu/component-library/playground/ec/?path=/story/${eclSection}-`;
+        const eclFinalUrl = `${eclPath + component}--${eclStory}`;
+        // Puppeteer will go to the url and try to click on the link in the left sidebar
+        // to reveal the available stories.
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(eclFinalUrl);
+        // Menu link.
+        if ((await page.$(`div#${eclSection}-${component}`)) !== null) {
+          await page.click(`div#${eclSection}-${component}`);
+          // All the links to the stories.
+          const stories = await page.$$(
+            `a#explorercomponents-${component} + div a`
+          );
+
+          if (stories) {
+            let hrefs = [];
+            for (const story of stories) {
+              const href = await page.evaluate(
+                el => el.getAttribute('href'),
+                story
+              );
+              // Try to match the urls found with the one built in this script.
+              if (eclFinalUrl === `https://ec.europa.eu${href}`) {
+                hrefs = false;
+                break;
+              }
+
+              const eclVariant = href.split('--')[1];
+              hrefs.push(eclVariant);
+            }
+            // We offer alternatives in case we cannot find the requested one.
+            if (hrefs.length > 0) {
+              console.error(
+                `We couldn't find the story "${eclStory}" for the "${component}" component in the ECL website, but we've found these alternatives: ${hrefs.join(
+                  ', '
+                )}`
+              );
+              process.exit(1);
+            }
+          }
+        }
+        // This will reveal the markup container.
+        await page.click('button[title="Show HTML"]');
 
         const eclTwigMarkup = fs
           .readFileSync(`${twigFullPath}/${fileName}`, 'utf-8')
@@ -141,23 +185,28 @@ yargsInteractive()
           el => el.innerHTML,
           await page.$('code')
         );
+        // The html we get is enriched by a syntax highlighter.
         eclMarkup = decode(eclMarkup.replace(/<\/?[^>]+(>|$)/g, ''));
         eclMarkup = eclMarkup.replace(/^<div>/, '');
         eclMarkup = eclMarkup.replace(/<\/div>$/, '');
-
+        // Make the diff against the php rendered files.
         const diff = htmlDiffer.diffHtml(eclMarkup, eclTwigMarkup);
         const isEqual = htmlDiffer.isEqual(eclMarkup, eclTwigMarkup);
+
         console.log(
           `Comparing ${fileName} with ECL markup from ${eclFinalUrl}:'`
         );
 
+        let successMsg = false;
         if (isEqual) {
-          res = '> Perfectly matching!*';
+          successMsg = '> Perfectly matching!*';
         } else {
-          res = logger.logDiffText(diff, { charsAroundDiff: 40 });
+          logger.logDiffText(diff, { charsAroundDiff: 40 });
         }
 
-        console.log(`${res}`);
+        if (successMsg) {
+          console.log(successMsg);
+        }
         console.log(
           `\n* For the diff we use https://www.npmjs.com/package/html-differ, with this conf:`
         );
