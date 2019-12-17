11c8679003a963bcd9dfcaac9e82b3bbae0fa6ea
planctus <davidepuntoferrante@gmail.com>
2019-12-17 08:54:36 +0100

    feat(ecl-diff): improvements - TWIG-180

--- php/php_storybook/scripts/ecl-diff.js
+++ php/php_storybook/scripts/ecl-diff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/* eslint-disable import/no-extraneous-dependencies, no-console */
+/* eslint-disable import/no-extraneous-dependencies, no-console, no-param-reassign, no-restricted-syntax, no-await-in-loop */
 
 const fs = require('fs');
 const htmlDiffer = require('html-differ');
@@ -10,6 +10,7 @@ const decode = require('decode-html');
 const yargsInteractive = require('yargs-interactive');
 
 const options = {
+  interactive: { default: true },
   component: {
     type: 'input',
     default: '',
@@ -20,7 +21,7 @@ const options = {
     default: '',
     describe: 'The name of the twig variant (what is after the --) ',
   },
-  story: {
+  eclStory: {
     type: 'input',
     default: '',
     describe:
@@ -44,20 +45,60 @@ const diffOptions = {
   ignoreDuplicateAttributes: false,
 };
 
+const getVariant = element => {
+  element = element.split('--')[1].replace(extension, '');
+  return element;
+};
+
 yargsInteractive()
-  .usage('$0 <command> [args]')
   .interactive(options)
   .then(result => {
     (async () => {
       try {
         let res = '';
         const { component } = result;
-        const { story } = result;
+        const { eclStory } = result;
         const { variant } = result;
-        const eclFinalUrl = `${eclPath + component}--${story}`;
+
+        if (!component) {
+          console.error(
+            `We need to know which component and variant you want to test`
+          );
+        }
+
+        const eclFinalUrl = `${eclPath + component}--${eclStory}`;
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(eclFinalUrl);
+        await page.click(`div#components-${component}`);
+        const stories = await page.$$(
+          `a#explorercomponents-${component} + div a`
+        );
+        if (stories) {
+          let hrefs = [];
+          for (const story of stories) {
+            const href = await page.evaluate(
+              el => el.getAttribute('href'),
+              story
+            );
+            if (eclFinalUrl === `https://ec.europa.eu${href}`) {
+              hrefs = false;
+              break;
+            }
+
+            const eclVariant = href.split('--')[1];
+            hrefs.push(eclVariant);
+          }
+
+          if (hrefs.length > 0) {
+            console.error(
+              `We couldn't find the variant ${eclStory} in the ECL website, but we've found these: ${hrefs.join(
+                ', '
+              )}`
+            );
+          }
+        }
+
         await page.click('button[title="Show HTML"]');
 
         let fileName = '';
@@ -66,13 +107,36 @@ yargsInteractive()
         } else {
           fileName = component + extension;
         }
-        const twigFullPath = `${systemFolder}/${component}/${fileName}`;
+        const twigFullPath = `${systemFolder}/${component}`;
         if (!fs.existsSync(twigFullPath)) {
           console.error(
-            `It seems that the component [${component}] you want to diff has not been rendere yet, please run yarn check:component ${component}`
+            `It seems that "${component}" has not been rendered yet, please run yarn check:component ${component}`
           );
+          process.exit(1);
+        } else if (!fs.existsSync(`${twigFullPath}/${fileName}`)) {
+          const files = fs.readdirSync(twigFullPath);
+          let phpFiles = files.filter(file => {
+            return (
+              file
+                .split('.')
+                .slice(1)
+                .join('.') === 'php.html'
+            );
+          });
+
+          phpFiles = phpFiles.map(getVariant);
+
+          console.error(
+            `It seems that "${variant}" has not been found, but we've found these: ${phpFiles.join(
+              ', '
+            )}`
+          );
+          process.exit(1);
         }
-        const eclTwigMarkup = fs.readFileSync(twigFullPath, 'utf-8').toString();
+
+        const eclTwigMarkup = fs
+          .readFileSync(`${twigFullPath}/${fileName}`, 'utf-8')
+          .toString();
         let eclMarkup = await page.evaluate(
           el => el.innerHTML,
           await page.$('code')
@@ -95,12 +159,13 @@ yargsInteractive()
 
         console.log(`${res}`);
         console.log(
-          `\n* The files we check are formatted with https://www.npmjs.com/package/prettier, for the diff we use https://www.npmjs.com/package/html-differ, with this conf:`
+          `\n* For the diff we use https://www.npmjs.com/package/html-differ, with this conf:`
         );
         console.log(diffOptions);
 
         process.exit(0);
       } catch (error) {
+        console.error(error.toString());
         process.exit(1);
       }
     })();
