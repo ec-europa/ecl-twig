6834c03d31c9d5acfaf6d1d4880e4f8542d47945
planctus <davidepuntoferrante@gmail.com>
2020-02-19 16:07:28 +0100

    fix(language-list): Renaming main template, fixing php-storybook, fixing template syntax - FRONT-823

--- php/php_storybook/scripts/render.js
+++ php/php_storybook/scripts/render.js
@@ -11,22 +11,16 @@ const extension = 'html.twig';
 const rootFolder = process.cwd();
 const distFolder = `${rootFolder}/php`;
 const systemFolder = `${distFolder}/packages/${system}`;
-
 const components = fs.readdirSync(systemFolder);
-
+// Loop in each component found.
 components.forEach(component => {
-  let componentTemplate = '';
   const pkg = `${system}-component-${component}`;
-  /* Three known exceptions.. */
+  let componentTemplate = component;
+  // Two known exceptions..
   if (component === 'checkbox' || component === 'radio') {
     componentTemplate = `${component}-group`;
-  } else {
-    componentTemplate = component;
   }
-  if (component === 'language-list') {
-    componentTemplate = `${component}-splash`;
-  }
-  /* This is the template we are going to render */
+  // This is the template we are going to render.
   const template = `@ecl-twig/${pkg}/ecl-${componentTemplate}.${extension}`;
   const dataFiles = fs.readdirSync(`${systemFolder}/${component}/specs`);
 
@@ -38,7 +32,7 @@ components.forEach(component => {
 
     const data = require(`${systemFolder}/${component}/specs/${dataFile}`);
 
-    /* Render with twing */
+    // Render with twing.
     let html = twing.render(template, data);
 
     if (component === 'inpage-navigation') {
