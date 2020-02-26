4094ece58dfd7a899463ef2a63f814d9113ed783
joosthe <hermans.joost@outlook.com>
2020-02-26 15:03:04 +0100

    fix(blockquote): progression commit - FRONT-803

--- src/ec/packages/ec-component-blockquote/blockquote.story.js
+++ src/ec/packages/ec-component-blockquote/blockquote.story.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text ,button } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
 
@@ -8,17 +8,43 @@ import data from '@ecl/ec-specs-blockquote/demo/data';
 import blockquote from './ecl-blockquote.html.twig';
 import notes from './README.md';
 
+// Labels for the group ids 
+const requiredGroupId = 'Mandatory elements';
+const optionalGroupId = 'Optional elements';
+const useCasesGroup = 'Use cases';
+
+
+
+// Prepare the knobs
+
+let toggleAuthor = false;
+const initialAutor= data.author;
 storiesOf('Components/Blockquote', module)
   .addDecorator(withKnobs)
   .addDecorator(withCode)
   .addDecorator(withNotes)
   .add(
     'default',
-    () =>
-      blockquote({
-        author: text('Author name', data.author),
-        citation: text('Citation', data.citation),
-      }),
+    () =>{
+   
+      if (toggleAuthor == true){
+        console.log('load this');
+        const dataAuthorBtnToggler =() => {
+          console.log('load this also');
+          
+          data.author = text('autor', '',optionalGroupId); 
+        }
+      }
+      const dataAuthorBtnToggler = () => {
+        data.author = text('autor', 'smldfmsldfkmsldfkmsldkf',optionalGroupId); 
+        toggleAuthor = true;
+      }
+      
+      button('use the author', dataAuthorBtnToggler, useCasesGroup);
+  //    button('Reset the layout', resetBtnToggler, useCasesGroup);
+      return blockquote(data);
+    },
+   
     {
       notes: { markdown: notes, json: data },
     }
