3529cba45dd8423bfc618b0e33731b344a5af53f
joosthe <hermans.joost@outlook.com>
2020-02-27 09:20:49 +0100

    fix(blockquote): progression commit - FRONT-803

--- src/ec/packages/ec-component-blockquote/blockquote.story.js
+++ src/ec/packages/ec-component-blockquote/blockquote.story.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text ,button } from '@storybook/addon-knobs';
+import { withKnobs, text, button } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
 
@@ -8,43 +9,55 @@ import data from '@ecl/ec-specs-blockquote/demo/data';
 import blockquote from './ecl-blockquote.html.twig';
 import notes from './README.md';
 
-// Labels for the group ids 
-const requiredGroupId = 'Mandatory elements';
+// Labels for the group ids
 const optionalGroupId = 'Optional elements';
+const requiredGroupId = 'Mandatory elements';
 const useCasesGroup = 'Use cases';
 
+// Preserve the adapted specs.
+const defaultData = { ...data };
 
+const authorBtnToggler = () => {
+  defaultData.author = defaultData.author ? false : data.author;
+};
+// const citationBtnToggler = () => {
+//   defaultData.citation = defaultData.citation ? false : data.citation;
+// };
 
-// Prepare the knobs
+// Reset buttons.
+// const resetBtnToggler = () => {
+//   defaultData = { ...data };
+// };
 
-let toggleAuthor = false;
-const initialAutor= data.author;
+const formatQuote = passeddata => {
+  const citation = passeddata.citation
+    ? text('Citation', passeddata.citation, requiredGroupId)
+    : false;
+
+  const author = passeddata.author
+    ? text('Author', passeddata.author, optionalGroupId)
+    : false;
+
+  const newQuote = {
+    author,
+    citation,
+  };
+
+  // Return the full object.
+  return newQuote;
+};
+
 storiesOf('Components/Blockquote', module)
   .addDecorator(withKnobs)
   .addDecorator(withCode)
   .addDecorator(withNotes)
   .add(
     'default',
-    () =>{
-   
-      if (toggleAuthor == true){
-        console.log('load this');
-        const dataAuthorBtnToggler =() => {
-          console.log('load this also');
-          
-          data.author = text('autor', '',optionalGroupId); 
-        }
-      }
-      const dataAuthorBtnToggler = () => {
-        data.author = text('autor', 'smldfmsldfkmsldfkmsldkf',optionalGroupId); 
-        toggleAuthor = true;
-      }
-      
-      button('use the author', dataAuthorBtnToggler, useCasesGroup);
-  //    button('Reset the layout', resetBtnToggler, useCasesGroup);
-      return blockquote(data);
+    () => {
+      button('With or without author', authorBtnToggler, useCasesGroup);
+
+      return blockquote(formatQuote(data));
     },
-   
     {
       notes: { markdown: notes, json: data },
     }
