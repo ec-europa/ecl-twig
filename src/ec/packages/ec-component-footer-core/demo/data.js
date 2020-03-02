cecc5e457ef28f90fcf85758fb098b93acbc09e8
planctus <davidepuntoferrante@gmail.com>
2020-03-02 12:53:11 +0100

    feat(footers): Aligning with 2.24.0 FRONT-835 (#348)
    * feat(use-cases): footer standardised - FRONT-820
    
    * feat[demo]: Footer harmonised, use-cases, mandatory, optional knobs - FRONT-822
    
    * feat(demo): Footer core - Adding mandatory elements knobs - FRONT-830
    
    * feat(demo) - Footer core, small fix - FRONT-830
    
    * feat(demo): Footer standardised, small fixes - FRONT-820
    
    * feat(demo): Footer core, improving label for no optional elements
    
    * feat(demo): Footer, Improving show-hiding in harmonised group3 - FRONT-822
    
    * feat(footer-harmonised): Using updated specs - FRONT-835
    
    * feat(footers): Aligning with 2.24.0, green diffs for them all - FRONT-835
    
    * feat(footers): Removing commented code - FRONT-835£
    
    * feat(footers): Using local variable in footer-core - FRONT-835
    
    * feat(footers): Using local variables in footer-harmonised - FRONT-835
    
    * feat(footers): Using local variables in footer standardised - FRONT-835
    
    * feat(footers): fixing knobs labels in footer harmonised - FRONT-835
    
    * feat(footers): missing description knob in footer core - FRONT-835
    
    Co-authored-by: Alexis Gaillard <a@bypopcorn.com>

--- src/ec/packages/ec-component-footer-core/demo/data.js
+++ src/ec/packages/ec-component-footer-core/demo/data.js
@@ -1,27 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies, no-param-reassign */
-import specData from '@ecl/ec-specs-footer-core/demo/data';
-import { formatLink } from '@ecl-twig/data-utils';
+import specs from '@ecl/ec-specs-footer-core/demo/data';
+import adapter from '../adapter';
 
-const adapter = initialData => {
-  // Copy reference specification demo data.
-  const adaptedData = JSON.parse(JSON.stringify(initialData));
-  adaptedData.sections.forEach(section => {
-    if (section.title) {
-      section.title = formatLink(section.title);
-    }
-    if (section.sectionClassName) {
-      section.section_class_name = section.sectionClassName;
-      delete section.sectionClassName;
-    }
-    if (section.listClassName) {
-      section.list_class_name = section.listClassName;
-      delete section.listClassName;
-    }
-    if (section.links && Array.isArray(section.links)) {
-      section.links = section.links.map(formatLink);
-    }
-  });
-  return adaptedData;
-};
-
-export default adapter(specData);
+export default adapter(specs);
