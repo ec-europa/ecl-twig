cf33709903fa521ffe26c864d9930cc78dde0b8d
Joosthe <hermans.joost@outlook.com>
2020-03-03 14:12:53 +0100

    feat(knobs): Blockquote panel with three sub-panels, buttons for show/hiding - FRONT-803 (#346)

--- src/ec/packages/ec-component-blockquote/blockquote.story.js
+++ src/ec/packages/ec-component-blockquote/blockquote.story.js
@@ -1,12 +1,24 @@
+/* eslint-disable no-param-reassign, no-shadow */
 import { storiesOf } from '@storybook/html';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
-
 import data from '@ecl/ec-specs-blockquote/demo/data';
-
 import blockquote from './ecl-blockquote.html.twig';
 import notes from './README.md';
+// Labels for the group ids
+const optionalGroupId = 'Optional elements';
+const requiredGroupId = 'Mandatory elements';
+
+const prepareQuote = prepareData => {
+  prepareData.citation = text(
+    'citation',
+    prepareData.citation,
+    requiredGroupId
+  );
+  prepareData.author = text('author', prepareData.author, requiredGroupId);
+  return prepareData;
+};
 
 storiesOf('Components/Blockquote', module)
   .addDecorator(withKnobs)
@@ -14,11 +26,16 @@ storiesOf('Components/Blockquote', module)
   .addDecorator(withNotes)
   .add(
     'default',
-    () =>
-      blockquote({
-        author: text('Author name', data.author),
-        citation: text('Citation', data.citation),
-      }),
+    () => {
+      const preparedData = { ...data };
+      const storyData = prepareQuote(preparedData);
+      storyData.demo = text(
+        'optional elements',
+        'No optional element in this story',
+        optionalGroupId
+      );
+      return blockquote(storyData);
+    },
     {
       notes: { markdown: notes, json: data },
     }
