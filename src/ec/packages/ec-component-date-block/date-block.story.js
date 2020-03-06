db23350b06c4008c90d209c95be52aa8d925adb0
planctus <davidepuntoferrante@gmail.com>
2020-03-06 10:56:57 +0100

    refactor(demo): Date block, mandatory/optional knobs, added stories for statuses - FRONT-900

--- src/ec/packages/ec-component-date-block/date-block.story.js
+++ src/ec/packages/ec-component-date-block/date-block.story.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign, no-shadow */
 import { storiesOf } from '@storybook/html';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
@@ -7,14 +8,28 @@ import data from './demo/data';
 import dateBlock from './ecl-date-block.html.twig';
 import notes from './README.md';
 
+// Labels for the groups.
+const requiredGroupId = 'Mandatory elements';
+const optionalGroupId = 'Optional elements';
+
 const options = {
   default: '',
   canceled: 'canceled',
   past: 'past',
   ongoing: 'ongoing',
 };
-const label = 'Variant';
+
 const defaultValue = 'default';
+
+const prepareDateBlock = data => {
+  data.day = text('day', data.day, requiredGroupId);
+  data.year = text('year', data.year, requiredGroupId);
+  data.month = text('month', data.month, requiredGroupId);
+  data.month_full = text('month_full', data.month_full, requiredGroupId);
+  data.date_time = text('date_time', data.date_time, optionalGroupId);
+
+  return data;
+};
 
 storiesOf('Components/Date block', module)
   .addDecorator(withKnobs)
@@ -23,14 +38,70 @@ storiesOf('Components/Date block', module)
   .add(
     'default',
     () => {
-      return dateBlock({
-        variant: select(label, options, defaultValue),
-        day: text('Day', data.day),
-        year: text('Year', data.year),
-        month: text('Month', data.month),
-        month_full: text('Full month', data.month_full),
-        date_time: text('Time', data.date_time),
-      });
+      const variant = select(
+        'variant',
+        [defaultValue],
+        defaultValue,
+        requiredGroupId
+      );
+      const dataStory = prepareDateBlock(data);
+      dataStory.variant = variant;
+
+      return dateBlock(dataStory);
+    },
+    {
+      notes: { markdown: notes, json: data },
+    }
+  )
+  .add(
+    'ongoing',
+    () => {
+      const variant = select(
+        'variant',
+        [options.ongoing],
+        'ongoing',
+        requiredGroupId
+      );
+      const dataStory = prepareDateBlock(data);
+      dataStory.variant = variant;
+
+      return dateBlock(dataStory);
+    },
+    {
+      notes: { markdown: notes, json: data },
+    }
+  )
+  .add(
+    'canceled',
+    () => {
+      const variant = select(
+        'variant',
+        [options.canceled],
+        'canceled',
+        requiredGroupId
+      );
+      const dataStory = prepareDateBlock(data);
+      dataStory.variant = variant;
+
+      return dateBlock(dataStory);
+    },
+    {
+      notes: { markdown: notes, json: data },
+    }
+  )
+  .add(
+    'past',
+    () => {
+      const variant = select(
+        'variant',
+        [options.past],
+        'past',
+        requiredGroupId
+      );
+      const dataStory = prepareDateBlock(data);
+      dataStory.variant = variant;
+
+      return dateBlock(dataStory);
     },
     {
       notes: { markdown: notes, json: data },
