29a0cffcf35478b9aa70ed5d93c9fa5ce6cd43cc
planctus <davidepuntoferrante@gmail.com>
2020-03-06 11:52:09 +0100

    refactor(demo): Checkbox, optional/mandatory knobs - FRONT-901

--- src/ec/packages/ec-component-checkbox/checkbox.story.js
+++ src/ec/packages/ec-component-checkbox/checkbox.story.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import {
+  withKnobs,
+  text,
+  boolean,
+  select,
+  object,
+} from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
 
@@ -9,9 +16,55 @@ import dataDefault from './demo/data--default';
 import checkboxGroup from './ecl-checkbox-group.html.twig';
 import notes from './README.md';
 
-dataDefault.items.forEach(item => {
-  item.icon_path = defaultSprite; // eslint-disable-line no-param-reassign
-});
+// Labels for the groups.
+const requiredGroupId = 'Mandatory elements';
+const optionalGroupId = 'Optional elements';
+
+const prepareCheckbox = data => {
+  data.label = text(
+    'label',
+    'Select your preferred destinations',
+    requiredGroupId
+  );
+  data.invalid_text = text(
+    'invalid_text',
+    dataDefault.invalid_text,
+    requiredGroupId
+  );
+  data.invalid = boolean('invalid', false, optionalGroupId);
+  data.required = boolean('required', false, optionalGroupId);
+  data.helper_text = text(
+    'helper_text',
+    dataDefault.helper_text,
+    optionalGroupId
+  );
+  data.optional_text = text('optional_text', '(optional)', optionalGroupId);
+  data.required_text = text('required_text', '*', optionalGroupId);
+  data.extra_classes = text('extra_classes', '', optionalGroupId);
+  data.extra_attributes = object(
+    'extra_attributes',
+    { name: '', value: '' },
+    optionalGroupId
+  );
+
+  data.items.forEach((item, i) => {
+    item.id = select(`items[${i}].id`, [item.id], item.id, requiredGroupId);
+    item.value = select(
+      `items[${i}].value`,
+      [item.value],
+      item.value,
+      requiredGroupId
+    );
+    item.icon_path = select(
+      `items[${i}]icon_path`,
+      [defaultSprite],
+      defaultSprite,
+      requiredGroupId
+    );
+  });
+
+  return data;
+};
 
 storiesOf('Components/Forms/Checkbox', module)
   .addDecorator(withKnobs)
@@ -19,20 +72,11 @@ storiesOf('Components/Forms/Checkbox', module)
   .addDecorator(withCode)
   .add(
     'default',
-    () =>
-      checkboxGroup({
-        ...dataDefault,
-        helper_text: text('Help message', dataDefault.helper_text),
-        invalid: boolean('Invalid', false),
-        invalid_text: text(
-          'Error message for the group',
-          dataDefault.invalid_text
-        ),
-        label: text('Legend', 'Select your preferred destinations'),
-        optional_text: text('Optional text', '(optional)'),
-        required: boolean('Required', false),
-        required_text: text('Required text', '*'),
-      }),
+    () => {
+      const data = prepareCheckbox(dataDefault);
+
+      return checkboxGroup(data);
+    },
     {
       notes: { markdown: notes, json: dataDefault },
     }
