fda48a1641d05b6900fbcba1f15a31d5c3db6f64
planctus <davidepuntoferrante@gmail.com>
2020-03-13 15:51:50 +0100

    refactor(demo): Link, grouping knobs FRONT-908

--- src/ec/packages/ec-component-link/link.story.js
+++ src/ec/packages/ec-component-link/link.story.js
@@ -1,4 +1,4 @@
-import merge from 'deepmerge';
+/* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
@@ -11,6 +11,10 @@ import dataCta from './demo/data--cta';
 import dataStandalone from './demo/data--standalone';
 import link from './ecl-link.html.twig';
 import notes from './README.md';
+
+// Labels for the groups.
+const requiredGroupId = 'Mandatory elements';
+const optionalGroupId = 'Optional elements';
 
 const iconsList = {};
 iconsList.none = null;
@@ -31,7 +35,85 @@ const iconPositionSettings = {
   before: 'before',
   after: 'after',
 };
-const defaultTransform = '';
+
+const prepareLink = data => {
+  data.link.href = select(
+    'link.path',
+    [data.link.path],
+    data.link.path,
+    requiredGroupId
+  );
+  data.link.type = select(
+    'link.type',
+    [data.link.type],
+    data.link.type,
+    requiredGroupId
+  );
+
+  data.extra_classes = text('extra_classes', '', optionalGroupId);
+  const attribute1Name = text('extra_attributes[0].name', '', optionalGroupId);
+  // First attribute.
+  if (attribute1Name !== '') {
+    data.extra_attributes = [];
+    let attribute = {};
+    const attribute1Value = text(
+      'extra_attributes[0].value',
+      '',
+      optionalGroupId
+    );
+    const attribute2Name = text(
+      'extra_attributes[1].name',
+      '',
+      optionalGroupId
+    );
+    attribute.name = attribute1Name;
+    if (attribute1Value !== '') {
+      attribute.value = attribute1Value;
+    }
+    data.extra_attributes.push(attribute);
+    // Second attribute.
+    if (attribute2Name !== '') {
+      const attribute2Value = text(
+        'extra_attributes[1].value',
+        '',
+        optionalGroupId
+      );
+      attribute = {};
+      attribute.name = attribute2Name;
+      if (attribute2Value !== '') {
+        attribute.value = attribute2Value;
+      }
+      data.extra_attributes.push(attribute);
+    }
+  } else {
+    delete data.extra_attributes;
+  }
+
+  return data;
+};
+
+const prepareIcon = (name, data) => {
+  const icon = {};
+  icon.name = name;
+  icon.type = select('icon.type', ['ui'], 'ui', optionalGroupId);
+  icon.path = select(
+    'icon.path',
+    [defaultSprite],
+    defaultSprite,
+    optionalGroupId
+  );
+  icon.size = select('icon.size', ['xs'], 'xs', optionalGroupId);
+  icon.transform = select('icon.transform', transforms, '', optionalGroupId);
+  if (icon) {
+    data.icon = icon;
+    data.link.icon_position = select(
+      'link.icon_position',
+      iconPositionSettings,
+      'after',
+      optionalGroupId
+    );
+  }
+};
 
 storiesOf('Components/Navigation/Link', module)
   .addDecorator(withKnobs)
@@ -40,33 +122,28 @@ storiesOf('Components/Navigation/Link', module)
   .add(
     'default',
     () => {
-      const iconsListSelect = select('Icon (sample)', iconsList, null);
-      const iconPosition = select(
-        'Icon position',
-        iconPositionSettings,
-        'after'
+      const dataStory = prepareLink(dataDefault);
+      dataStory.link.label = text(
+        'link.label',
+        dataStory.link.label,
+        requiredGroupId
       );
-      const transform = select('Transform', transforms, defaultTransform);
-
-      const linkData = merge(dataDefault, {
-        link: {
-          label: text('Label', 'Default link'),
-          icon_position: iconPosition,
-        },
-        icon: {
-          type: 'ui',
-          name: iconsListSelect,
-          path: defaultSprite,
-          size: 'fluid',
-          transform,
-        },
-      });
+      dataStory.link.type = select(
+        'link.type',
+        [dataStory.link.type],
+        dataStory.link.type,
+        requiredGroupId
+      );
+      const name = select('icon.name', iconsList, null, optionalGroupId);
+      if (name !== null) {
+        prepareIcon(name, dataStory);
+      }
 
       const demo = document.createDocumentFragment();
       const wrapper = document.createElement('p');
       wrapper.className = 'ecl-u-type-paragraph';
       wrapper.setAttribute('demo_only', true);
-      wrapper.innerHTML = link(linkData);
+      wrapper.innerHTML = link(dataStory);
       demo.appendChild(wrapper);
 
       return demo;
@@ -78,29 +155,24 @@ storiesOf('Components/Navigation/Link', module)
   .add(
     'standalone',
     () => {
-      const iconsListSelect = select('Icon (sample)', iconsList, null);
-      const iconPosition = select(
-        'Icon position',
-        iconPositionSettings,
-        'after'
+      const dataStory = prepareLink(dataStandalone);
+      dataStory.link.label = text(
+        'link.label',
+        'Standalone link',
+        requiredGroupId
       );
-      const transform = select('Transform', transforms, defaultTransform);
+      dataStory.link.type = select(
+        'link.type',
+        [dataStory.link.type],
+        dataStory.link.type,
+        requiredGroupId
+      );
+      const name = select('icon.name', iconsList, null, optionalGroupId);
+      if (name !== null) {
+        prepareIcon(name, dataStory);
+      }
 
-      return link(
-        merge(dataStandalone, {
-          link: {
-            label: text('Label', 'Standalone link'),
-            icon_position: iconPosition,
-          },
-          icon: {
-            type: 'ui',
-            name: iconsListSelect,
-            path: defaultSprite,
-            size: 'fluid',
-            transform,
-          },
-        })
-      );
+      return link(dataStory);
     },
     {
       notes: { markdown: notes, json: dataStandalone },
@@ -109,24 +181,18 @@ storiesOf('Components/Navigation/Link', module)
   .add(
     'cta',
     () => {
-      const iconsListSelect = select('Icon (sample)', iconsList, null);
-      const transform = select('Transform', transforms, defaultTransform);
-
-      return link(
-        merge(dataCta, {
-          link: {
-            label: text('Label', 'Call to action link'),
-            icon_position: 'after',
-          },
-          icon: {
-            name: iconsListSelect,
-            path: defaultSprite,
-            type: 'ui',
-            transform,
-            size: 'fluid',
-          },
-        })
+      const dataStory = prepareLink(dataCta);
+      dataStory.link.label = text(
+        'link.label',
+        'Call to action link',
+        requiredGroupId
       );
+      const name = select('icon.name', iconsList, null, optionalGroupId);
+      if (name !== null) {
+        prepareIcon(name, dataStory);
+      }
+
+      return link(dataStory);
     },
     {
       notes: { markdown: notes, json: dataCta },
