16c75b6678a4a24481db6856e552bfb8a8375890
planctus <davidepuntoferrante@gmail.com>
2020-03-16 15:26:48 +0100

    refactor(demo): Search form, grouping knobs - FRONT-907 (#367)

--- src/ec/packages/ec-component-search-form/demo/data.js
+++ src/ec/packages/ec-component-search-form/demo/data.js
@@ -3,21 +3,13 @@ import specs from '@ecl/ec-specs-search-form/demo/data';
 const data = {
   text_input: {
     id: specs.textInputId,
-    extra_classes: 'ecl-search-form__text-input',
     label: specs.inputLabel,
-    type: 'search',
   },
   button: {
-    variant: 'search',
     icon: {
-      type: 'general',
-      name: 'search',
       path: '/icons.svg',
-      size: 'xs',
     },
     label: specs.buttonLabel,
-    extra_classes: 'ecl-search-form__button',
-    extra_attributes: [{ name: 'aria-label', value: 'Search' }],
   },
 };
 
