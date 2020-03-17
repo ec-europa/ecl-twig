b122963def78d9323cc850902758626a87525583
planctus <davidepuntoferrante@gmail.com>
2020-03-17 09:03:24 +0100

    refactor(demo): Select, using knobs from the story-utils plugin - FRONT-906

--- src/ec/packages/ec-component-select/select.story.js
+++ src/ec/packages/ec-component-select/select.story.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
+import {
+  getExtraKnobs,
+  getFormKnobs,
+  buttonLabels,
+} from '@ecl-twig/story-utils';
 
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import specData from './demo/data';
@@ -10,107 +15,28 @@ import specData from './demo/data';
 import selectBox from './ecl-select.html.twig';
 import notes from './README.md';
 
-const inputWidthOptions = {
-  small: 's',
-  medium: 'm',
-  large: 'l',
-};
-
-// Labels for the groups.
-const requiredGroupId = 'Mandatory elements';
-const optionalGroupId = 'Optional elements';
-const statesGroupId = 'States';
-
 const prepareSelect = data => {
-  data.invalid = boolean('invalid', false, statesGroupId);
-  data.disabled = boolean('disabled', false, statesGroupId);
-  data.required = boolean('required', false, statesGroupId);
-  data.label = text('label', data.label, requiredGroupId);
-  if (data.invalid) {
-    data.invalid_text = text(
-      'invalid_text',
-      data.invalid_text,
-      requiredGroupId
-    );
-  } else {
-    data.invalid_text = text(
-      'invalid_text',
-      data.invalid_text,
-      optionalGroupId
-    );
-  }
-  if (data.required) {
-    data.required_text = text(
-      'required_text',
-      data.required_text,
-      requiredGroupId
-    );
-    data.optional_text = text(
-      'optional text',
-      data.optional_text,
-      optionalGroupId
-    );
-  } else {
-    data.required_text = text(
-      'required_text',
-      data.required_text,
-      optionalGroupId
-    );
-    data.optional_text = text(
-      'optional text',
-      data.optional_text,
-      requiredGroupId
-    );
-  }
+  getFormKnobs(data);
+  getExtraKnobs(data);
+
   data.icon_path = select(
     'icon_path',
     [defaultSprite],
     defaultSprite,
-    requiredGroupId
+    buttonLabels.required
   );
-  data.helper_text = text('helper_text', data.helper_text, optionalGroupId);
-  data.width = select('width', inputWidthOptions, 'm', optionalGroupId);
-  data.extra_classes = text('extra_classes', '', optionalGroupId);
-  const attribute1Name = text('extra_attributes[0].name', '', optionalGroupId);
-  // First attribute.
-  if (attribute1Name !== '') {
-    data.extra_attributes = [];
-    let attribute = {};
-    const attribute1Value = text(
-      'extra_attributes[0].value',
-      '',
-      optionalGroupId
-    );
-    const attribute2Name = text(
-      'extra_attributes[1].name',
-      '',
-      optionalGroupId
-    );
-    attribute.name = attribute1Name;
-    if (attribute1Value !== '') {
-      attribute.value = attribute1Value;
-    }
-    data.extra_attributes.push(attribute);
-    // Second attribute.
-    if (attribute2Name !== '') {
-      const attribute2Value = text(
-        'extra_attributes[1].value',
-        '',
-        optionalGroupId
-      );
-      attribute = {};
-      attribute.name = attribute2Name;
-      if (attribute2Value !== '') {
-        attribute.value = attribute2Value;
-      }
-      data.extra_attributes.push(attribute);
-    }
-  } else {
-    delete data.extra_attributes;
-  }
+
   data.options.forEach((option, i) => {
-    option.label = text(`options[${i}].label`, option.label, requiredGroupId);
-    option.value = text(`options[${i}].value`, option.value, requiredGroupId);
+    option.label = text(
+      `options[${i}].label`,
+      option.label,
+      buttonLabels.required
+    );
+    option.value = text(
+      `options[${i}].value`,
+      option.value,
+      buttonLabels.required
+    );
   });
 
   return data;
