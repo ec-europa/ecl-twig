ca993a8e598675ea344a6c2e5196e381e18c7487
planctus <davidepuntoferrante@gmail.com>
2020-03-18 10:52:59 +0100

    refactor(demo): Text Area, States, optional/mandatory knobs - FRONT-904 (#364)

--- src/ec/packages/ec-component-text-area/text-area.story.js
+++ src/ec/packages/ec-component-text-area/text-area.story.js
@@ -1,42 +1,24 @@
+/* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
+import { getExtraKnobs, getFormKnobs } from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
 import specData from './demo/data--default';
 import textArea from './ecl-text-area.html.twig';
 import notes from './README.md';
 
-const inputWidthOptions = {
-  small: 's',
-  medium: 'm',
-  large: 'l',
+const prepareTextArea = data => {
+  getFormKnobs(data);
+  getExtraKnobs(data);
+
+  return data;
 };
 
 storiesOf('Components/Forms/Text area', module)
   .addDecorator(withKnobs)
   .addDecorator(withNotes)
   .addDecorator(withCode)
-  .add(
-    'default',
-    () => {
-      const inputWidthSelect = select('Width', inputWidthOptions, 'm');
-
-      return textArea({
-        label: text('Label', specData.label),
-        rows: text('Rows', specData.rows),
-        helper_text: text('Help message', specData.helper_text),
-        invalid: boolean('Invalid', false),
-        invalid_text: text('Invalid text', specData.invalid_text),
-        disabled: boolean('Disabled', false),
-        required: boolean('Required', specData.required),
-        required_text: text('Required Text', specData.required_text),
-        optional_text: text('Optional text', specData.optional_text),
-        width: inputWidthSelect,
-        id: 'example-textarea-id-1',
-        name: 'example-name',
-      });
-    },
-    {
-      notes: { markdown: notes },
-    }
-  );
+  .add('default', () => textArea(prepareTextArea(specData)), {
+    notes: { markdown: notes, json: specData },
+  });
