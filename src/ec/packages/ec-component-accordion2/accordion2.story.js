a095ec15dcaa9eade70a4a9218d1ef5894f9d89e
joosthe <hermans.joost@outlook.com>
2020-03-18 15:03:08 +0100

    refactor(demo): progression - FRONT-910

--- src/ec/packages/ec-component-accordion2/accordion2.story.js
+++ src/ec/packages/ec-component-accordion2/accordion2.story.js
@@ -3,12 +3,25 @@ import { storiesOf } from '@storybook/html';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
-
+import { buttonLabels } from '@ecl-twig/story-utils';
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import demoData from './demo/data';
 
 import accordion2 from './ecl-accordion2.html.twig';
 import notes from './README.md';
+
+const PrepareAccordion2 = data => {
+  data.items.forEach(item => {
+    const { id, content, toggle } = item;
+    item.toggle.label = text(
+      `label ${id}`,
+      toggle.label,
+      buttonLabels.required
+    );
+    item.content = text(`content ${id}`, content, buttonLabels.required);
+    item.toggle.icon.path = defaultSprite;
+  });
+};
 
 storiesOf('Components/Accordion2', module)
   .addDecorator(withKnobs)
@@ -17,18 +30,8 @@ storiesOf('Components/Accordion2', module)
   .add(
     'default',
     () => {
-      // This needs to be in the scope of this function.
-      // Called on knob's change of value.
-      demoData.items.forEach(item => {
-        const { id, content, toggle } = item;
-
-        item.toggle.label = text(`Label ${id}`, toggle.label);
-        item.content = text(`Content ${id}`, content);
-
-        item.toggle.icon.path = defaultSprite;
-      });
-
-      return accordion2(demoData);
+      const accordion2DemoData = PrepareAccordion2(demoData);
+      return accordion2(accordion2DemoData);
     },
     {
       notes: { markdown: notes, json: demoData },
