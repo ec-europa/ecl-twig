e4373198f86de9737da9d27b7dff9d73266ee6b7
joosthe <hermans.joost@outlook.com>
2020-03-20 09:55:59 +0100

    refactor(knobs): progression commit - FRONT-913

--- src/ec/packages/ec-component-page-banner/page-banner.story.js
+++ src/ec/packages/ec-component-page-banner/page-banner.story.js
@@ -3,8 +3,13 @@ import { storiesOf } from '@storybook/html';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 import withCode from '@ecl-twig/storybook-addon-code';
-import { getExtraKnobs, buttonLabels } from '@ecl-twig/story-utils';
-import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
+import {
+  getExtraKnobs,
+  buttonLabels,
+  getIconKnobs,
+} from '@ecl-twig/story-utils';
+
+import uiIcons from '@ecl/ec-resources-icons/dist/lists/ui.json';
 
 import bannerDataDefault from './demo/data--default';
 import bannerDataImage from './demo/data--image';
@@ -15,14 +20,13 @@ import bannerDataAlignLeft from './demo/data--align-left';
 import pageBanner from './ecl-page-banner.html.twig';
 import notes from './README.md';
 
+uiIcons.unshift('null');
 const preparePageBanner = data => {
-  if (data.link.link.label) {
-    data.link.link.label = text(
-      'Link label',
-      data.link.link.label,
-      buttonLabels.required
-    );
+  if (data.centered) {
+    data.centered = boolean('centered', data.centered, buttonLabels.states);
   }
+
+  data.type = select('type', [data.type], data.type, buttonLabels.required);
 
   if (data.title) {
     data.title = text('title', data.title, buttonLabels.required);
@@ -32,21 +36,45 @@ const preparePageBanner = data => {
     data.baseline = text('baseline', data.baseline, buttonLabels.required);
   }
 
-  if (data.centered) {
-    data.centered = boolean('centered', data.centered, buttonLabels.optional);
-  }
-
   if (data.image) {
     data.image = text('image', data.image, buttonLabels.optional);
   }
-  if (data.link.icon) {
-    data.link.icon.path = select(
-      'link.icon.path',
-      [defaultSprite],
-      defaultSprite,
+
+  if (data.link.link.label) {
+    data.link.link.label = text(
+      'Link label',
+      data.link.link.label,
       buttonLabels.required
-    ); // eslint-disable-line no-param-reassign
+    );
   }
+
+  data.link.link.path = text(
+    'link.link.path',
+    data.link.link.path,
+    buttonLabels.required
+  );
+
+  data.link.icon.name = select(
+    'link.icon.name',
+    uiIcons,
+    data.link.icon.name,
+    buttonLabels.optional
+  );
+
+  if (data.link.icon.name !== 'null') {
+    getIconKnobs(
+      data,
+      data.link.icon.name,
+      'ui',
+      'xs',
+      'default',
+      'rotate-90',
+      true
+    );
+  } else {
+    delete data.link.icon;
+  }
+
   getExtraKnobs(data);
   return data;
 };
@@ -55,95 +83,22 @@ storiesOf('Components/Banners/Page Banner', module)
   .addDecorator(withNotes)
   .addDecorator(withCode)
   .addDecorator(withKnobs)
-  .add(
-    'image',
-    () => {
-      bannerDataImage.type = select(
-        'type',
-        [bannerDataImage.type],
-        bannerDataImage.type,
-        buttonLabels.optional
-      );
-      const pageBannerImage = preparePageBanner(bannerDataImage);
-      return pageBanner(pageBannerImage);
-    },
-    {
-      notes: { markdown: notes, json: preparePageBanner(bannerDataImage) },
-    }
-  )
+  .add('image', () => preparePageBanner(pageBanner(bannerDataImage)), {
+    notes: { markdown: notes, json: bannerDataImage },
+  })
   .add(
     'image-shade',
-    () => {
-      bannerDataImageShade.type = select(
-        'type',
-        [bannerDataImageShade.type],
-        bannerDataImageShade.type,
-        buttonLabels.optional
-      );
-      const pageBannerImageShade = preparePageBanner(bannerDataImageShade);
-      return pageBanner(pageBannerImageShade);
-    },
+    () => preparePageBanner(pageBanner(bannerDataImageShade)),
     {
-      notes: {
-        markdown: notes,
-        json: preparePageBanner(bannerDataImageShade),
-      },
+      notes: { markdown: notes, json: bannerDataImageShade },
     }
   )
-  .add(
-    'primary',
-    () => {
-      bannerDataPrimary.type = select(
-        'type',
-        [bannerDataPrimary.type],
-        bannerDataPrimary.type,
-        buttonLabels.optional
-      );
-      const pageBannerPrimary = preparePageBanner(bannerDataPrimary);
-      return pageBanner(pageBannerPrimary);
-    },
-    {
-      notes: {
-        markdown: notes,
-        json: preparePageBanner(bannerDataPrimary),
-      },
-    }
-  )
-  .add(
-    'default',
-    () => {
-      bannerDataDefault.type = select(
-        'type',
-        [bannerDataDefault.type],
-        bannerDataDefault.type,
-        buttonLabels.optional
-      );
-      const pageBannerDefault = preparePageBanner(bannerDataDefault);
-      return pageBanner(pageBannerDefault);
-    },
-    {
-      notes: {
-        markdown: notes,
-        json: preparePageBanner(bannerDataDefault),
-      },
-    }
-  )
-  .add(
-    'align-left',
-    () => {
-      bannerDataAlignLeft.type = select(
-        'type',
-        [bannerDataAlignLeft.type],
-        bannerDataAlignLeft.type,
-        buttonLabels.optional
-      );
-      const pageBannerLeft = preparePageBanner(bannerDataAlignLeft);
-      return pageBanner(pageBannerLeft);
-    },
-    {
-      notes: {
-        markdown: notes,
-        json: preparePageBanner(bannerDataAlignLeft),
-      },
-    }
-  );
+  .add('primary', () => preparePageBanner(pageBanner(bannerDataPrimary)), {
+    notes: { markdown: notes, json: bannerDataPrimary },
+  })
+  .add('default', () => preparePageBanner(pageBanner(bannerDataDefault)), {
+    notes: { markdown: notes, json: bannerDataDefault },
+  })
+  .add('align-left', () => preparePageBanner(pageBanner(bannerDataAlignLeft)), {
+    notes: { markdown: notes, json: bannerDataAlignLeft },
+  });
