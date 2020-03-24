1ccddf6b81215df8e49c0ee7c80a83112848859a
joosthe <hermans.joost@outlook.com>
2020-03-24 11:07:01 +0100

    refactor(knobs): progression commit - FRONT-923

--- src/ec/packages/ec-component-timeline/timeline.story.js
+++ src/ec/packages/ec-component-timeline/timeline.story.js
@@ -1,47 +1,71 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable no-param-reassign,prefer-destructuring */
 
 import { storiesOf } from '@storybook/html';
-import { withKnobs, number } from '@storybook/addon-knobs';
+import { withKnobs, number, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
+import { getExtraKnobs, buttonLabels } from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
-import iconPath from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 
 import demoData from './demo/data';
 
 import timeline from './ecl-timeline.html.twig';
 import notes from './README.md';
 
+const prepareTimeline = data => {
+  const from = data.hide.from;
+  const to = data.hide.to;
+
+  let hiddenCount = 0;
+  if (to > 0) {
+    hiddenCount = to - from;
+  } else {
+    hiddenCount = data.items.length + to - from;
+  }
+  data.toggle_collapsed = select(
+    `Show ${hiddenCount} more items`,
+    [`Show ${hiddenCount} more items`],
+    `Show ${hiddenCount} more items`,
+    buttonLabels.required
+  );
+  data.toggle_expanded = select(
+    `Hide ${hiddenCount} items`,
+    [`Hide ${hiddenCount} items`],
+    `Hide ${hiddenCount} items`,
+    buttonLabels.required
+  );
+  data.hide.from = number(
+    'hide.from',
+    data.hide.from,
+    {},
+    buttonLabels.required
+  );
+  data.hide.to = number('hide.to', data.hide.to, {}, buttonLabels.required);
+  data.items.forEach((item, index) => {
+    const { id, label, title, content } = item;
+    item.id = text(`item[${index}].id`, id, buttonLabels.required);
+    item.label = text(`item[${index}].label`, label, buttonLabels.required);
+    item.title = text(`item[${index}].title`, title, buttonLabels.required);
+    // this needs to be looked at
+    item.content = text(
+      `item[${index}].content`,
+      content,
+      buttonLabels.required
+    );
+  });
+  data.icon_path = select(
+    `icon_path`,
+    [data.icon_path],
+    data.icon_path,
+    buttonLabels.required
+  );
+  getExtraKnobs(data);
+  return data;
+};
+
 storiesOf('Components/Timeline', module)
   .addDecorator(withKnobs)
   .addDecorator(withCode)
   .addDecorator(withNotes)
-  .add(
-    'default',
-    () => {
-      const from = number('From', demoData.hide.from);
-      const to = number('To', demoData.hide.to);
-
-      let hiddenCount = 0;
-      if (to > 0) {
-        hiddenCount = to - from;
-      } else {
-        hiddenCount = demoData.items.length + to - from;
-      }
-
-      const fullDemoData = {
-        ...demoData,
-        toggle_collapsed: `Show ${hiddenCount} more items`,
-        toggle_expanded: `Hide ${hiddenCount} items`,
-        icon_path: iconPath,
-        hide: {
-          from,
-          to,
-        },
-      };
-
-      return timeline(fullDemoData);
-    },
-    {
-      notes: { markdown: notes, json: demoData },
-    }
-  );
+  .add('default', () => timeline(prepareTimeline(demoData)), {
+    notes: { markdown: notes, json: demoData },
+  });
