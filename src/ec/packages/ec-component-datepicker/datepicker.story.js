28811bffe68a7dcc6c5d24d2de42acf18a2ed937
joosthe <hermans.joost@outlook.com>
2020-03-25 14:35:17 +0100

    refactor(knobs): added knobs - FRONT-905

--- src/ec/packages/ec-component-datepicker/datepicker.story.js
+++ src/ec/packages/ec-component-datepicker/datepicker.story.js
@@ -1,13 +1,55 @@
 /* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 import withCode from '@ecl-twig/storybook-addon-code';
+import { getExtraKnobs, buttonLabels } from '@ecl-twig/story-utils';
 
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
-import data from './demo/data';
+import dataDefault from './demo/data';
 import datepicker from './ecl-datepicker.html.twig';
 import notes from './README.md';
+
+const prepareDatePicker = data => {
+  data.label = text('label', data.label, buttonLabels.required);
+  data.icons_path = select(
+    'icons_path',
+    [defaultSprite],
+    defaultSprite,
+    buttonLabels.required
+  );
+  data.helper_text = text(
+    'helper_text',
+    data.helper_text,
+    buttonLabels.required
+  );
+  data.invalid = boolean('invalid', false, buttonLabels.cases);
+  data.invalid_text = text(
+    'invalid_text',
+    data.invalid_text,
+    buttonLabels.optional
+  );
+  data.disabled = boolean('disabled', false, buttonLabels.cases);
+  data.required = boolean('required', data.required, buttonLabels.cases);
+  data.required_text = text(
+    'required_text',
+    data.required_text,
+    buttonLabels.optional
+  );
+  data.optional_text = text(
+    'optional_text',
+    data.optional_text,
+    buttonLabels.optional
+  );
+  data.placeholder = text(
+    'placeholder',
+    data.placeholder,
+    buttonLabels.required
+  );
+  getExtraKnobs(data);
+
+  return data;
+};
 
 storiesOf('Components/Forms/Datepicker', module)
   .addDecorator(withNotes)
@@ -16,20 +58,9 @@ storiesOf('Components/Forms/Datepicker', module)
   .add(
     'default',
     () => {
-      return datepicker({
-        label: text('Label', data.label),
-        icons_path: defaultSprite,
-        helper_text: text('Helper text', data.helper_text),
-        invalid: boolean('Invalid', false),
-        invalid_text: text('Invalid text', data.invalid_text),
-        disabled: boolean('Disabled', false),
-        required: boolean('Required', data.required),
-        required_text: text('Required text', data.required_text),
-        optional_text: text('Optional text', data.optional_text),
-        placeholder: text('Placeholder', data.placeholder),
-      });
+      return datepicker(prepareDatePicker(dataDefault));
     },
     {
-      notes: { markdown: notes, json: data },
+      notes: { markdown: notes, json: dataDefault },
     }
   );
