fa7841dde5a5bd7a36b7a5afbd31eb173722ab39
joosthe <hermans.joost@outlook.com>
2020-03-25 10:40:13 +0100

    refactor(knobs): changes done after review - FRONT-923

--- src/ec/packages/ec-component-timeline/timeline.story.js
+++ src/ec/packages/ec-component-timeline/timeline.story.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-param-reassign,prefer-destructuring */
-
 import { storiesOf } from '@storybook/html';
 import { withKnobs, number, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import { getExtraKnobs, buttonLabels } from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
+import iconPath from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import he from 'he';
 
 import demoData from './demo/data';
@@ -16,20 +16,19 @@ const prepareTimeline = data => {
   const from = data.hide.from;
   const to = data.hide.to;
   let hiddenCount = 0;
-
   if (to > 0) {
     hiddenCount = to - from;
   } else {
     hiddenCount = data.items.length + to - from;
   }
   data.toggle_collapsed = select(
-    `Show ${hiddenCount} more items`,
+    `data.toggle_collapsed`,
     [`Show ${hiddenCount} more items`],
     `Show ${hiddenCount} more items`,
     buttonLabels.required
   );
   data.toggle_expanded = select(
-    `Hide ${hiddenCount} items`,
+    `data.toggle_expanded`,
     [`Hide ${hiddenCount} items`],
     `Hide ${hiddenCount} items`,
     buttonLabels.required
@@ -46,15 +45,14 @@ const prepareTimeline = data => {
     item.id = text(`items[${index}].id`, id, buttonLabels.required);
     item.label = text(`items[${index}].label`, label, buttonLabels.required);
     item.title = text(`items[${index}].title`, title, buttonLabels.required);
-    // this needs to be looked at
     item.content = he.decode(
       text(`items[${index}].content`, content, buttonLabels.required)
     );
   });
   data.icon_path = select(
     `icon_path`,
-    [data.icon_path],
-    data.icon_path,
+    [iconPath],
+    iconPath,
     buttonLabels.required
   );
   getExtraKnobs(data);
