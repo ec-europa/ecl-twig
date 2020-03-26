5d5a309a2bb43e8a74f513db97b152a1129d8da7
planctus <davidepuntoferrante@gmail.com>
2020-03-26 15:46:28 +0100

    refactor(demo): Language list, optional/mandatory knobs, fixing the notes plugin - FRONT-925

--- src/ec/packages/ec-component-language-list/demo/data--splash.js
+++ src/ec/packages/ec-component-language-list/demo/data--splash.js
@@ -1,12 +1,6 @@
 import specDataSplash from '@ecl/ec-specs-language-list/demo/data--splash';
-import { formatLinkAlt } from '@ecl-twig/data-utils';
+import adapter from '../adapter';
 
-export default {
-  items: specDataSplash.items.map(formatLinkAlt),
-  overlay: false,
-  icon_path: '/icons.svg',
-  logo: {
-    alt: specDataSplash.logoAlt,
-    path: '/logo--mute.svg',
-  },
-};
+specDataSplash.overlay = false;
+
+export default adapter(specDataSplash);
