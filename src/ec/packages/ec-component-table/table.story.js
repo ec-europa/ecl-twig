a32eb4ac2517af00266c74a31e58c9514ea0ab43
planctus <davidepuntoferrante@gmail.com>
2020-03-31 14:38:39 +0200

    refactor(table): optional/mandatory knobs - FRONT-924

--- src/ec/packages/ec-component-table/table.story.js
+++ src/ec/packages/ec-component-table/table.story.js
@@ -10,32 +10,93 @@ import dataMulti from '@ecl/ec-specs-table/demo/data--multi';
 import table from './ecl-table.html.twig';
 import notes from './README.md';
 
-const prepareTable = data => {
-  data.zebra = boolean('zebra', false, buttonLabels.cases);
-  data.headers[0].forEach((header, index) => {
-    header.label = text(
-      `headers[0][${index}].label`,
-      header.label,
-      buttonLabels.required
-    );
-    header.colspan = text(
-      `headers[0][${index}].colspan`,
-      header.colspan,
-      buttonLabels.optional
-    );
+// Preserve original data
+const defaultData = { ...dataDefault };
+const dataZebra = { ...dataDefault, zebra: true };
+
+defaultData.rows.forEach(rows => {
+  rows.forEach(row => {
+    row.extra_attributes = [
+      { name: 'data-test' },
+      { name: 'data-test-another' },
+    ];
   });
-  data.rows[0].forEach((row, index) => {
-    row.label = text(
-      `rows[0][${index}].label`,
-      row.label,
-      buttonLabels.required
-    );
-    row.extra_classes = text(
-      `rows[0][${index}].extra_classes`,
-      row.extra_classes,
-      buttonLabels.optional
-    );
-    row.group = boolean(`rows[0][${index}].group`, false, buttonLabels.cases);
+});
+
+const prepareTable = data => {
+  data.zebra = boolean('zebra', data.zebra, buttonLabels.cases);
+  data.headers.forEach((headers, i) => {
+    headers.forEach((header, ind) => {
+      header.label = text(
+        `headers[${i}][${ind}].label`,
+        header.label,
+        buttonLabels.required
+      );
+      header.colspan = text(
+        `headers[${i}][${ind}].colspan`,
+        header.colspan,
+        buttonLabels.optional
+      );
+    });
+  });
+
+  data.rows.forEach((rows, i) => {
+    rows.forEach((row, ind) => {
+      row.label = text(
+        `rows[${i}][${ind}].label`,
+        row.label,
+        buttonLabels.required
+      );
+      row['data-ecl-table-header'] = text(
+        `rows[${i}][${ind}]['data-ecl-table-header']`,
+        row['data-ecl-table-header'],
+        buttonLabels.required
+      );
+      row.extra_classes = text(
+        `rows[${i}][${ind}].extra_classes`,
+        row.extra_classes,
+        buttonLabels.optional
+      );
+      if (!row.extra_attributes) {
+        row.extra_attributes = [];
+        row.extra_attributes[0] = {};
+        row.extra_attributes[1] = {};
+      }
+      row.extra_attributes[0].name = text(
+        `rows[${i}][${ind}].extra_attributes[0].name`,
+        row.extra_attributes[0].name,
+        buttonLabels.optional
+      );
+      row.extra_attributes[0].value = text(
+        `rows[${i}][${ind}].extra_attributes[0].value`,
+        row.extra_attributes[0].value,
+        buttonLabels.optional
+      );
+      if (row.extra_attributes[1].name) {
+        row.extra_attributes[1].name = text(
+          `rows[${i}][${ind}].extra_attributes[1].name`,
+          row.extra_attributes[1].name,
+          buttonLabels.optional
+        );
+        row.extra_attributes[1].value = text(
+          `rows[${i}][${ind}].extra_attributes[1].value`,
+          row.extra_attributes[1].value,
+          buttonLabels.optional
+        );
+      }
+      row.group = boolean(
+        `rows[${i}][${ind}].group`,
+        row.group,
+        buttonLabels.optional
+      );
+      if (data.rows[i][ind].group) {
+        row['data-ecl-table-header-group'] = text(
+          `rows[${i}][${ind}]['data-ecl-table-header-group']`,
+          row['data-ecl-table-header-group'],
+          buttonLabels.optional
+        );
+      }
+    });
   });
 
   getExtraKnobs(data);
@@ -47,58 +108,19 @@ storiesOf('Components/Table', module)
   .addDecorator(withNotes)
   .addDecorator(withCode)
   .addDecorator(withKnobs)
-  .add(
-    'default',
-    () => {
-      dataDefault.rows.forEach(row => {
-        delete row.extra_attributes;
-      });
-
-      return table(prepareTable(dataDefault));
-    },
-    {
-      notes: { markdown: notes, json: dataDefault },
-    }
-  )
+  .add('default', () => table(prepareTable(dataDefault)), {
+    notes: { markdown: notes, json: dataDefault },
+  })
   .add(
     'default with row extra attributes',
-    () => {
-      dataDefault.rows.forEach((row, index) => {
-        row.extra_attributes = text(
-          `rows[${index}].extra_attributes`,
-          'data-test data-test-another',
-          buttonLabels.optional
-        ); // eslint-disable-line no-param-reassign
-      });
-
-      return table(prepareTable(dataDefault));
-    },
+    () => table(prepareTable(defaultData)),
     {
-      notes: { markdown: notes, json: prepareTable(dataDefault) },
+      notes: { markdown: notes, json: defaultData },
     }
   )
-  .add(
-    'Zebra',
-    () => {
-      dataDefault.rows.forEach(row => {
-        delete row.extra_attributes;
-      });
-
-      return table(dataDefault);
-    },
-    {
-      notes: { markdown: notes, json: dataDefault },
-    }
-  )
-  .add(
-    'Multi',
-    () => {
-      dataMulti.rows.forEach(row => {
-        delete row.extra_attributes;
-      });
-      return table(dataMulti);
-    },
-    {
-      notes: { markdown: notes, json: dataMulti },
-    }
-  );
+  .add('Zebra', () => table(prepareTable(dataZebra)), {
+    notes: { markdown: notes, json: dataZebra },
+  })
+  .add('Multi', () => table(prepareTable(dataMulti)), {
+    notes: { markdown: notes, json: dataMulti },
+  });
