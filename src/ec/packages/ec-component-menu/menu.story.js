c86c116dc57d4b80499c1b760a638b90462aed46
planctus <davidepuntoferrante@gmail.com>
2020-03-31 09:47:04 +0200

    feat(menuV2): Implementation, green diff with ECL - FRONT-991 (#394)

--- src/ec/packages/ec-component-menu/menu.story.js
+++ src/ec/packages/ec-component-menu/menu.story.js
@@ -1,26 +1,41 @@
-import merge from 'deepmerge';
+/* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
+import { withKnobs, select, text, object } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
+import { getExtraKnobs, buttonLabels } from '@ecl-twig/story-utils';
+
 import iconPath from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
-
 import enData from './demo/data--en';
-
+import frData from './demo/data--fr';
 import menu from './ecl-menu.html.twig';
 import notes from './README.md';
 
+const prepareMenu = data => {
+  data.title = text('title', data.title, buttonLabels.required);
+  data.close = text('close', data.close, buttonLabels.required);
+  data.back = text('back', data.back, buttonLabels.required);
+  data.icon_path = select(
+    'icon_path',
+    [iconPath],
+    iconPath,
+    buttonLabels.required
+  );
+
+  getExtraKnobs(data);
+
+  data.items = object('items', data.items, buttonLabels.optional);
+
+  return data;
+};
+
 storiesOf('Components/Navigation/Menu', module)
+  .addDecorator(withKnobs)
   .addDecorator(withNotes)
   .addDecorator(withCode)
-  .add(
-    'default',
-    () =>
-      menu(
-        merge(enData, {
-          icon_path: iconPath,
-        })
-      ),
-    {
-      notes: { markdown: notes, json: enData },
-    }
-  );
+  .add('default', () => menu(prepareMenu(enData)), {
+    notes: { markdown: notes, json: enData },
+  })
+  .add('translated', () => menu(prepareMenu(frData)), {
+    notes: { markdown: notes, json: frData },
+  });
