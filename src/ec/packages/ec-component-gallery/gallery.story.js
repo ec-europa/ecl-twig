fae293f70e46891677a05060922b66216207d4c4
planctus <davidepuntoferrante@gmail.com>
2020-03-31 18:13:21 +0200

    refactor(gallery): optional/mandatory knobs, progress - FRONT-926

--- src/ec/packages/ec-component-gallery/gallery.story.js
+++ src/ec/packages/ec-component-gallery/gallery.story.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { withKnobs, text, select, object } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import { getExtraKnobs, buttonLabels } from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
@@ -11,58 +11,63 @@ import gallery from './ecl-gallery.html.twig';
 import dataDefault from './demo/data';
 import notes from './README.md';
 
-const preperaGallery = data => {
-  data.items.forEach((item, index) => {
+const prepareGallery = data => {
+  data.items.forEach((item, i) => {
+    item.extra_attributes = [];
     item.share_path = select(
-      `items[${index}].share_path`,
+      `items[${i}].share_path`,
       [item.share_path],
       item.share_path,
       buttonLabels.required
     );
     item.description = text(
-      `items[${index}].description`,
+      `items[${i}].description`,
       item.description,
+      buttonLabels.optional
+    );
+    item.meta = text(`items[${i}].meta`, item.meta, buttonLabels.optional);
+    item.alt = text(`items[${i}].alt`, item.alt, buttonLabels.optional);
+    item.extra_classes = text(
+      `items[${i}].extra_classes`,
+      item.extra_classes,
+      buttonLabels.optional
+    );
+    item.extra_attributes[0] = {};
+    item.extra_attributes[0].name = text(
+      `items[${i}].extra_attributes[0].name`,
+      item.extra_attributes[0].name,
+      buttonLabels.optional
+    );
+    item.extra_attributes[0].value = text(
+      `items[${i}].extra_attributes[0].value`,
+      item.extra_attributes[0].value,
       buttonLabels.optional
     );
     if (item.icon) {
       item.icon.path = select(
-        `items[${index}].icon.path`,
+        `items[${i}].icon.path`,
         [defaultSprite],
         defaultSprite,
         buttonLabels.required
       );
     }
+    if (item.video) {
+      item.video.sources[0].src = select(
+        `items[${i}].video.sources[0].src`,
+        [item.video.sources[0].src],
+        item.video.sources[0].src,
+        buttonLabels.required
+      );
+    }
   });
-  data.overlay.close.icon.path = select(
-    'overlay.close.icon.path',
-    [defaultSprite],
-    defaultSprite,
-    buttonLabels.required
+
+  data.selected_item_id = text(
+    'selected_item_id',
+    data.selected_item_id,
+    buttonLabels.optional
   );
-  data.overlay.previous.icon.path = select(
-    'overlay.previous.icon.path',
-    [defaultSprite],
-    defaultSprite,
-    buttonLabels.required
-  );
-  data.overlay.next.icon.path = select(
-    'overlay.next.icon.path',
-    [defaultSprite],
-    defaultSprite,
-    buttonLabels.required
-  );
-  data.overlay.download.icon.path = select(
-    'overlay.download.icon.path',
-    [defaultSprite],
-    defaultSprite,
-    buttonLabels.required
-  );
-  data.overlay.share.icon.path = select(
-    'overlay.share.icon.path',
-    [defaultSprite],
-    defaultSprite,
-    buttonLabels.required
-  );
+  data.overlay = object('data.overlay', data.overlay, buttonLabels.optional);
+
   getExtraKnobs(data);
 
   return data;
@@ -72,6 +77,6 @@ storiesOf('Components/Gallery', module)
   .addDecorator(withKnobs)
   .addDecorator(withCode)
   .addDecorator(withNotes)
-  .add('default', () => gallery(preperaGallery(dataDefault)), {
+  .add('default', () => gallery(prepareGallery(dataDefault)), {
     notes: { markdown: notes, json: dataDefault },
   });
