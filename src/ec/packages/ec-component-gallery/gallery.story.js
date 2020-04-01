fca780f79b7cd124a35a42af376bb004e70270dd
planctus <davidepuntoferrante@gmail.com>
2020-04-01 10:53:46 +0200

    refactor(demo): Gallery, optional/mandatory knobs, fixing template - FRONT-926

--- src/ec/packages/ec-component-gallery/gallery.story.js
+++ src/ec/packages/ec-component-gallery/gallery.story.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text, select, object } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import { getExtraKnobs, buttonLabels } from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
@@ -13,7 +13,6 @@ import notes from './README.md';
 
 const prepareGallery = data => {
   data.items.forEach((item, i) => {
-    item.extra_attributes = [];
     item.share_path = select(
       `items[${i}].share_path`,
       [item.share_path],
@@ -32,10 +31,12 @@ const prepareGallery = data => {
       item.extra_classes,
       buttonLabels.optional
     );
-    item.extra_attributes[0] = {};
+    if (!item.extra_attributes) {
+      item.extra_attributes = [{}];
+    }
     item.extra_attributes[0].name = text(
       `items[${i}].extra_attributes[0].name`,
-      item.extra_attributes[0].name,
+      '',
       buttonLabels.optional
     );
     item.extra_attributes[0].value = text(
@@ -43,6 +44,12 @@ const prepareGallery = data => {
       item.extra_attributes[0].value,
       buttonLabels.optional
     );
+    if (!item.extra_attributes[0].value) {
+      delete item.extra_attributes[0].value;
+    }
+    if (item.extra_attributes[0].name === '') {
+      delete item.extra_attributes;
+    }
     if (item.icon) {
       item.icon.path = select(
         `items[${i}].icon.path`,
@@ -66,7 +73,91 @@ const prepareGallery = data => {
     data.selected_item_id,
     buttonLabels.optional
   );
-  data.overlay = object('data.overlay', data.overlay, buttonLabels.optional);
+  // Overlay knobs.
+  data.overlay.counter_separator = text(
+    'overlay.counter_separator',
+    data.overlay.counter_separator,
+    buttonLabels.required
+  );
+  data.overlay.extra_classes = text(
+    'data.overlay.extra_classes',
+    '',
+    buttonLabels.optional
+  );
+  if (!data.overlay.extra_attributes) {
+    data.overlay.extra_attributes = [{}];
+  }
+  data.overlay.extra_attributes[0].name = text(
+    'data.overlay.extra_attributes[0].name',
+    '',
+    buttonLabels.optional
+  );
+  data.overlay.extra_attributes[0].value = text(
+    `data.overlay.extra_attributes[0].value`,
+    data.overlay.extra_attributes[0].value,
+    buttonLabels.optional
+  );
+  if (!data.overlay.extra_attributes[0].value) {
+    delete data.overlay.extra_attributes[0].value;
+  }
+  if (data.overlay.extra_attributes[0].name === '') {
+    delete data.overlay.extra_attributes;
+  }
+  data.overlay.close.label = text(
+    'overlay.close.label',
+    data.overlay.close.label,
+    buttonLabels.required
+  );
+  data.overlay.close.icon.path = select(
+    'overlay.close.icon.path',
+    [defaultSprite],
+    defaultSprite,
+    buttonLabels.required
+  );
+  data.overlay.previous.label = text(
+    'overlay.previous.label',
+    data.overlay.previous.label,
+    buttonLabels.required
+  );
+  data.overlay.previous.icon.path = select(
+    'overlay.previous.icon.path',
+    [defaultSprite],
+    defaultSprite,
+    buttonLabels.required
+  );
+  data.overlay.next.label = text(
+    'overlay.next.label',
+    data.overlay.next.label,
+    buttonLabels.required
+  );
+  data.overlay.next.icon.path = select(
+    'overlay.next.icon.path',
+    [defaultSprite],
+    defaultSprite,
+    buttonLabels.required
+  );
+  data.overlay.download.link.label = text(
+    'overlay.download.link.label',
+    data.overlay.download.link.label,
+    buttonLabels.required
+  );
+  data.overlay.download.link.path = text(
+    'overlay.download.link.path',
+    data.overlay.download.link.path,
+    buttonLabels.required
+  );
+  data.overlay.download.icon.path = select(
+    'overlay.download.icon.path',
+    [defaultSprite],
+    defaultSprite,
+    buttonLabels.required
+  );
+  data.overlay.share.icon.path = select(
+    'overlay.share.icon.path',
+    [defaultSprite],
+    defaultSprite,
+    buttonLabels.required
+  );
 
   getExtraKnobs(data);
 
