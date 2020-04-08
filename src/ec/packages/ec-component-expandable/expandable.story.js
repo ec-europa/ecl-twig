d7325272f98416623961b34476939c504fdf9480
planctus <davidepuntoferrante@gmail.com>
2020-04-08 10:54:59 +0200

    refactor(demo): Expandable, optional.mandatory knobs - FRONT-1021 (#399)

--- src/ec/packages/ec-component-expandable/expandable.story.js
+++ src/ec/packages/ec-component-expandable/expandable.story.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-param-reassign */
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
+import { getExtraKnobs, tabLabels } from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
 
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
@@ -9,24 +11,35 @@ import demoData from './demo/data';
 import expandable from './ecl-expandable.html.twig';
 import notes from './README.md';
 
+const prepareExpandable = data => {
+  data.id = select('id', [data.id], data.id, tabLabels.required);
+  data.label_expanded = text(
+    'label_expanded',
+    data.label_expanded,
+    tabLabels.required
+  );
+  data.label_collapsed = text(
+    'label_collapsed',
+    data.label_collapsed,
+    tabLabels.required
+  );
+  data.content = text('content', data.content, tabLabels.required);
+  data.button.icon.path = select(
+    'button.icon.path',
+    [defaultSprite],
+    defaultSprite,
+    tabLabels.required
+  );
+
+  getExtraKnobs(data);
+
+  return data;
+};
+
 storiesOf('Components/Expandables', module)
   .addDecorator(withKnobs)
   .addDecorator(withCode)
   .addDecorator(withNotes)
-  .add(
-    'default',
-    () => {
-      demoData.button.icon.path = defaultSprite;
-      demoData.label_expanded = text('Label Expanded', demoData.labelExpanded);
-      demoData.label_collapsed = text(
-        'Label Collapsed',
-        demoData.labelCollapsed
-      );
-      demoData.content = text('Content', demoData.content);
-
-      return expandable(demoData);
-    },
-    {
-      notes: { markdown: notes },
-    }
-  );
+  .add('default', () => expandable(prepareExpandable(demoData)), {
+    notes: { markdown: notes, json: demoData },
+  });
