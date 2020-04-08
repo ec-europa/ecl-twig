2d66a7b387ebb5b6b2fe2a2a80749b45cd2f02bd
planctus <davidepuntoferrante@gmail.com>
2020-04-08 10:59:39 +0200

    refactor(form-elements) checkbox, fixed template, adding knobs- FRONT-1029

--- src/ec/packages/ec-component-checkbox/checkbox.story.js
+++ src/ec/packages/ec-component-checkbox/checkbox.story.js
@@ -2,7 +2,12 @@
 import { storiesOf } from '@storybook/html';
 import { withKnobs, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
-import { getExtraKnobs, getFormKnobs, tabLabels } from '@ecl-twig/story-utils';
+import {
+  getExtraKnobs,
+  getFormGroupKnobs,
+  tabLabels,
+  getFormItemKnobs,
+} from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
 
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
@@ -11,25 +16,20 @@ import checkboxGroup from './ecl-checkbox-group.html.twig';
 import notes from './README.md';
 
 const prepareCheckbox = data => {
-  getFormKnobs(data);
+  getFormGroupKnobs(data);
+
+  getFormItemKnobs(data, true);
 
   data.items.forEach((item, i) => {
-    item.id = select(`items[${i}].id`, [item.id], item.id, tabLabels.required);
-    item.value = select(
-      `items[${i}].value`,
-      [item.value],
-      item.value,
-      tabLabels.required
-    );
     item.icon_path = select(
-      `items[${i}]icon_path`,
+      `items[${i}].icon_path`,
       [defaultSprite],
       defaultSprite,
       tabLabels.required
     );
   });
 
-  getExtraKnobs(data);
+  getExtraKnobs(data, true);
 
   return data;
 };
