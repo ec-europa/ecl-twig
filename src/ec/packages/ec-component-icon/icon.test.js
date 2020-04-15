b955341c90218f2f3d9285cccd84282efdc3acce
planctus <davidepuntoferrante@gmail.com>
2020-04-15 15:21:04 +0200

    feat(ecl-compliance): Applying _compliance_ parameter to included templates in link, validation for icon added - FRONT-253

--- src/ec/packages/ec-component-icon/icon.test.js
+++ src/ec/packages/ec-component-icon/icon.test.js
@@ -108,4 +108,20 @@ describe('EC - Icon', () => {
       return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
     });
   });
+  describe('with validation enabled and missing input data', () => {
+    test('returns the right warning message', () => {
+      expect.assertions(1);
+
+      const options = {
+        icon: {
+          name: 'check-filled',
+          path: '',
+          type: '',
+        },
+        _compliance_: true,
+      };
+
+      return expect(render(options)).resolves.toMatchSnapshot();
+    });
+  });
 });
