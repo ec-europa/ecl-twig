2d6485ae1359d889bc0a81d9a48fced404f98248
planctus <davidepuntoferrante@gmail.com>
2020-04-23 10:36:31 +0200

    feat(ecl-compliance): Skip link, adding child template - FRONT-1107

--- src/ec/packages/ec-component-skip-link/skip-link.story.js
+++ src/ec/packages/ec-component-skip-link/skip-link.story.js
@@ -3,17 +3,17 @@ import { storiesOf } from '@storybook/html';
 import { withKnobs, text, button } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
+import {
+  getExtraKnobs,
+  tabLabels,
+  getComplianceKnob,
+} from '@ecl-twig/story-utils';
 
 import specs from '@ecl/ec-specs-skip-link/demo/data';
 import skipLink from './ecl-skip-link.html.twig';
 import notes from './README.md';
 
-// Labels for the groups.
-const requiredGroupId = 'Mandatory elements';
-const optionalGroupId = 'Optional elements';
-const statesGroupId = 'States';
-
-// Buttons for the demo.
+// Button for the demo.
 const btnTabLabel = 'Focus on/off';
 const btnTabHandler = () => {
   const skipLinkEl = document.querySelector('.ecl-skip-link');
@@ -27,46 +27,12 @@ const btnTabHandler = () => {
 };
 
 const prepareSkipLink = data => {
-  data.label = text('label', data.label, requiredGroupId);
-  data.href = text('href', data.href, requiredGroupId);
-  data.extra_classes = text('extra_classes', '', optionalGroupId);
-  const attribute1Name = text('extra_attributes[0].name', '', optionalGroupId);
-  // First attribute.
-  if (attribute1Name !== '') {
-    data.extra_attributes = [];
-    let attribute = {};
-    const attribute1Value = text(
-      'extra_attributes[0].value',
-      '',
-      optionalGroupId
-    );
-    const attribute2Name = text(
-      'extra_attributes[1].name',
-      '',
-      optionalGroupId
-    );
-    attribute.name = attribute1Name;
-    if (attribute1Value !== '') {
-      attribute.value = attribute1Value;
-    }
-    data.extra_attributes.push(attribute);
-    // Second attribute.
-    if (attribute2Name !== '') {
-      const attribute2Value = text(
-        'extra_attributes[1].value',
-        '',
-        optionalGroupId
-      );
-      attribute = {};
-      attribute.name = attribute2Name;
-      if (attribute2Value !== '') {
-        attribute.value = attribute2Value;
-      }
-      data.extra_attributes.push(attribute);
-    }
-  } else {
-    delete data.extra_attributes;
-  }
+  data.label = text('label', data.label, tabLabels.required);
+  data.href = text('href', data.href, tabLabels.required);
+
+  getExtraKnobs(data);
+  getComplianceKnob(data);
+  button(btnTabLabel, btnTabHandler, tabLabels.states);
 
   return data;
 };
@@ -75,15 +41,6 @@ storiesOf('Components/Navigation/Skip Link', module)
   .addDecorator(withKnobs)
   .addDecorator(withNotes)
   .addDecorator(withCode)
-  .add(
-    'default',
-    () => {
-      button(btnTabLabel, btnTabHandler, statesGroupId);
-      const dataStory = prepareSkipLink(specs);
-
-      return skipLink(dataStory);
-    },
-    {
-      notes: { markdown: notes, json: specs },
-    }
-  );
+  .add('default', () => skipLink(prepareSkipLink(specs)), {
+    notes: { markdown: notes, json: specs },
+  });
