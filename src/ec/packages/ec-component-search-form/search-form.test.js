c6bbdfa89733b158bb5372cd8e6a22ad38a41dcc
planctus <davidepuntoferrante@gmail.com>
2020-04-28 11:47:42 +0200

    feat(ecl-compliance): Search form, adding child template - FRONT-1072 (#412)

--- src/ec/packages/ec-component-search-form/search-form.test.js
+++ src/ec/packages/ec-component-search-form/search-form.test.js
@@ -46,4 +46,22 @@ describe('EC - Search Form', () => {
       return expect(render(withExtraFormElements)).resolves.toMatchSnapshot();
     });
   });
+  describe('with validation enabled and missing input data', () => {
+    test('returns the right warning message', () => {
+      expect.assertions(1);
+
+      const dataCompliance = {
+        ...data,
+        button: {
+          label: '',
+        },
+        icon: {
+          path: '',
+        },
+        _compliance_: true,
+      };
+
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
+  });
 });
