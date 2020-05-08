c942a6e334b27d3259c6bc3f6528f58b958a2d4a
planctus <davidepuntoferrante@gmail.com>
2020-05-08 09:54:00 +0200

    feat(ecl-compliance): Tag, fixing knobs - FRONT-1076

--- src/ec/packages/ec-component-tag/tag.story.js
+++ src/ec/packages/ec-component-tag/tag.story.js
@@ -8,6 +8,7 @@ import {
   tabLabels,
   getComplianceKnob,
 } from '@ecl-twig/story-utils';
+
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import dataLink from './demo/data--link';
 import dataButton from './demo/data--button';
@@ -16,7 +17,7 @@ import tag from './ecl-tag.html.twig';
 import notes from './README.md';
 
 // Preserve the adapted specs.
-const prepareTag = data => {
+const prepareTag = (data, link, aria) => {
   data.tag.type = select(
     'tag.type',
     [data.tag.type],
@@ -24,10 +25,10 @@ const prepareTag = data => {
     tabLabels.required
   );
   data.tag.label = text('tag.label', data.tag.label, tabLabels.required);
-  if (data.tag.path) {
+  if (link) {
     data.tag.path = text('tag.path', data.tag.path, tabLabels.required);
   }
-  if (data.tag.aria_label) {
+  if (aria) {
     data.tag.aria_label = text(
       'tag.aria_label',
       data.tag.aria_label,
@@ -53,12 +54,12 @@ storiesOf('Components/Tag', module)
   .addDecorator(withKnobs)
   .addDecorator(withNotes)
   .addDecorator(withCode)
-  .add('as a link', () => tag(prepareTag(dataLink)), {
+  .add('as a link', () => tag(prepareTag(dataLink, true)), {
     notes: { markdown: notes, json: dataLink },
   })
   .add('as a button', () => tag(prepareTag(dataButton)), {
     notes: { markdown: notes, json: dataButton },
   })
-  .add('removable', () => tag(prepareTag(dataRemovable)), {
+  .add('removable', () => tag(prepareTag(dataRemovable, false, true)), {
     notes: { markdown: notes, json: dataRemovable },
   });
