a148ec1b19642a69884c116b11fc2ac7e4d4ae3c
planctus <davidepuntoferrante@gmail.com>
2020-05-25 12:32:19 +0200

    feat(ecl-compliance): Page Header Core, adding child template - FRONT-1087 (#455)
    * feat(ecl-compliance): Page Header Core, adding child template - FRONT-1087
    
    * Feat(ecl-compliance): Page Header Core, fixing templates - FRONT-1087
    
    * feat(ecl-compliance): Page Header Core, better handling knobs - FRONT-1087
    
    Co-authored-by: Kalin Chernev <kalin.chernev@gmail.com>

--- src/ec/packages/ec-component-page-header-core/page-header-core.story.js
+++ src/ec/packages/ec-component-page-header-core/page-header-core.story.js
@@ -2,6 +2,11 @@ import { storiesOf } from '@storybook/html';
 import { withKnobs, text, object } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
 import withCode from '@ecl-twig/storybook-addon-code';
+import {
+  getExtraKnobs,
+  tabLabels,
+  getComplianceKnob,
+} from '@ecl-twig/story-utils';
 
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
 import demoTitleContent from './demo/data--title';
@@ -11,61 +16,24 @@ import demoMetaTitleDescriptionContent from './demo/data--meta-title-description
 import pageHeaderCore from './ecl-page-header-core.html.twig';
 import notes from './README.md';
 
-// Labels for the groups.
-const requiredGroupId = 'Mandatory elements';
-const optionalGroupId = 'Optional elements';
-
-const preparePageHeaderCore = data => {
+const preparePageHeaderCore = (data, desc, meta) => {
   data.breadcrumb.icon_file_path = defaultSprite;
-  data.title = text('title', data.title, requiredGroupId);
-  data.breadcrumb = object('breadcrumb', data.breadcrumb, requiredGroupId);
+  data.title = text('title', data.title, tabLabels.required);
+  data.breadcrumb = object('breadcrumb', data.breadcrumb, tabLabels.required);
 
-  if (data.meta) {
-    data.meta = text('meta', data.meta, optionalGroupId);
+  if (meta) {
+    data.meta = text('meta', data.meta, tabLabels.optional);
   }
-  if (data.description) {
-    data.description = text('description', data.description, optionalGroupId);
-  }
-
-  data.extra_classes = text('extra_classes', '', optionalGroupId);
-
-  const attribute1Name = text('extra_attributes[0].name', '', optionalGroupId);
-
-  if (attribute1Name !== '') {
-    data.extra_attributes = [];
-    let attribute = {};
-    const attribute1Value = text(
-      'extra_attributes[0].value',
-      '',
-      optionalGroupId
+  if (desc) {
+    data.description = text(
+      'description',
+      data.description,
+      tabLabels.optional
     );
-    const attribute2Name = text(
-      'extra_attributes[1].name',
-      '',
-      optionalGroupId
-    );
-    attribute.name = attribute1Name;
-    if (attribute1Value !== '') {
-      attribute.value = attribute1Value;
-    }
-    data.extra_attributes.push(attribute);
-
-    if (attribute2Name !== '') {
-      const attribute2Value = text(
-        'extra_attributes[1].value',
-        '',
-        optionalGroupId
-      );
-      attribute = {};
-      attribute.name = attribute2Name;
-      if (attribute2Value !== '') {
-        attribute.value = attribute2Value;
-      }
-      data.extra_attributes.push(attribute);
-    }
-  } else {
-    delete data.extra_attributes;
   }
+
+  getExtraKnobs(data);
+  getComplianceKnob(data);
 
   return data;
 };
@@ -74,35 +42,23 @@ storiesOf('Components/Page Headers/Page Header Core', module)
   .addDecorator(withNotes)
   .addDecorator(withCode)
   .addDecorator(withKnobs)
-  .add(
-    'title',
-    () => {
-      const data = preparePageHeaderCore(demoTitleContent);
-
-      return pageHeaderCore(data);
-    },
-    {
-      notes: { markdown: notes, json: demoTitleContent },
-    }
-  )
+  .add('title', () => pageHeaderCore(preparePageHeaderCore(demoTitleContent)), {
+    notes: { markdown: notes, json: demoTitleContent },
+  })
   .add(
     'meta-title',
-    () => {
-      const data = preparePageHeaderCore(demoMetaTitleContent);
-
-      return pageHeaderCore(data);
-    },
+    () =>
+      pageHeaderCore(preparePageHeaderCore(demoMetaTitleContent, false, true)),
     {
       notes: { markdown: notes, json: demoMetaTitleContent },
     }
   )
   .add(
     'meta-title-description',
-    () => {
-      const data = preparePageHeaderCore(demoMetaTitleDescriptionContent);
-
-      return pageHeaderCore(data);
-    },
+    () =>
+      pageHeaderCore(
+        preparePageHeaderCore(demoMetaTitleDescriptionContent, true, true)
+      ),
     {
       notes: {
         markdown: notes,
