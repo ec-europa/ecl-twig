d73c0cbda8a6b8ab480d132233d1cb59027bcd5f
planctus <davidepuntoferrante@gmail.com>
2020-05-25 10:33:25 +0200

    feat(ecl-compliance): Site Header Harmonised, fixing test - FRONT-1092

--- src/ec/packages/ec-component-site-header-harmonised/site-header-harmonised.test.js
+++ src/ec/packages/ec-component-site-header-harmonised/site-header-harmonised.test.js
@@ -107,27 +107,27 @@ describe('EC - Site Header Harmonised', () => {
 
       return expect(render(withExtraClasses)).resolves.toMatchSnapshot();
     });
-  });
 
-  test('renders correctly with extra attributes', () => {
-    expect.assertions(1);
+    test('renders correctly with extra attributes', () => {
+      expect.assertions(1);
 
-    const withExtraAttributes = merge(dataGroup3, {
-      extra_attributes: [
-        { name: 'data-test', value: 'data-test-value' },
-        { name: 'data-test-1', value: 'data-test-value-1' },
-      ],
+      const withExtraAttributes = merge(dataGroup3, {
+        extra_attributes: [
+          { name: 'data-test', value: 'data-test-value' },
+          { name: 'data-test-1', value: 'data-test-value-1' },
+        ],
+      });
+
+      return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
     });
 
-    return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
-  });
+    test('with missing input data and debug enabled returns the right warning message', () => {
+      expect.assertions(1);
 
-  test('with missing input data and debug enabled returns the right warning message', () => {
-    expect.assertions(1);
+      const dataCompliance = { ...dataGroup3, _compliance_: true };
+      dataCompliance.site_name = '';
 
-    const dataCompliance = { ...dataGroup3, _compliance_: true };
-    dataCompliance.site_name = '';
-
-    return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
   });
 });
