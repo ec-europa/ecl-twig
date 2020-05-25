e8906756ff9066800d1639363fe410c850fa154b
planctus <davidepuntoferrante@gmail.com>
2020-05-25 09:00:09 +0200

    feat(ecl-compliance): Page Header Harmonised, adding child template FRONT-1149 (#457)
    * feat(ecl-compliance): Page Header Harmonised, adding child template - FRONT-1149
    
    * feat(ecl-compliance): Page Header Harmonised, fixing template, udating snapshot - FRONT-1149£
    
    * feat(ecl-compliance): Page Header Harmonised, better handling knobs - FRONT-1149

--- src/ec/packages/ec-component-page-header-harmonised/page-header-harmonised.test.js
+++ src/ec/packages/ec-component-page-header-harmonised/page-header-harmonised.test.js
@@ -37,6 +37,15 @@ describe('EC - Page Header Harmonised', () => {
 
       return expect(render(withExtraAttributes)).resolves.toMatchSnapshot();
     });
+
+    test('with missing input data and debug enabled it returns the right warning messages', () => {
+      expect.assertions(1);
+
+      const dataCompliance = { ...demoTitleContent, _compliance_: true };
+      dataCompliance.title = '';
+
+      return expect(render(dataCompliance)).resolves.toMatchSnapshot();
+    });
   });
 
   describe('meta-title', () => {
