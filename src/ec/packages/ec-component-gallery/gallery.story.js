623b676787e6f82d6874eed324f6c8d315cbf3ea
planctus <davidepuntoferrante@gmail.com>
2020-05-26 18:21:57 +0200

    feat(ecl-compliance): Gallery, adding child templates - FRONT-1075

--- src/ec/packages/ec-component-gallery/gallery.story.js
+++ src/ec/packages/ec-component-gallery/gallery.story.js
@@ -1,7 +1,11 @@
 import { storiesOf } from '@storybook/html';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withNotes } from '@ecl-twig/storybook-addon-notes';
-import { getExtraKnobs, tabLabels } from '@ecl-twig/story-utils';
+import {
+  getExtraKnobs,
+  tabLabels,
+  getComplianceKnob,
+} from '@ecl-twig/story-utils';
 import withCode from '@ecl-twig/storybook-addon-code';
 
 import defaultSprite from '@ecl/ec-resources-icons/dist/sprites/icons.svg';
@@ -158,6 +162,7 @@ const prepareGallery = data => {
   );
 
   getExtraKnobs(data);
+  getComplianceKnob(data);
 
   return data;
 };
