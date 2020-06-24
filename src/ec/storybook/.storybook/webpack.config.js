472f13c56954cfdafa994608245fbdf144292881
planctus <davidepuntoferrante@gmail.com>
2020-06-24 17:10:09 +0200

    feat(storybook): Ec deploy only for twing, trying to make the build silent again - FRONT-1115

--- src/ec/storybook/.storybook/webpack.config.js
+++ src/ec/storybook/.storybook/webpack.config.js
@@ -12,8 +12,8 @@ module.exports = ({ config: defaultConfig, mode }) => {
   });
   // Make it less verbose
   if (mode === 'PRODUCTION') {
-    // Remove ProgressPlugin (4th plugin)
-    const plugin = defaultConfig.plugins.splice(3, 1);
+    // Remove ProgressPlugin (5th plugin)
+    const plugin = defaultConfig.plugins.splice(4, 1);
     if (plugin[0].constructor.name !== 'ProgressPlugin') {
       defaultConfig.plugins.push(plugin[0]);
     }
