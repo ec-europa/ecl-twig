f94825ec36a4ee7e7f9f56eafe3ac29b30cb6fd0
planctus <davidepuntoferrante@gmail.com>
2020-09-01 09:12:40 +0200

    adding test and fixing diff to demonstrate that the rendering only fails in the browser

--- src/ec/packages/ec-component-layout/layout.test.js
+++ src/ec/packages/ec-component-layout/layout.test.js
@@ -2,8 +2,8 @@ import { merge, renderTwigFileAsNode } from '@ecl-twig/test-utils';
 
 import demoData from './demo/data';
 
-describe('EC - Date Block', () => {
-  const template = '@ecl-twig/ec-component-date-block/ecl-date-block.html.twig';
+describe('EC - Layout', () => {
+  const template = '@ecl-twig/ec-component-layout/ecl-layout.html.twig';
   const render = (params) => renderTwigFileAsNode(template, params);
 
   test('renders correctly', () => {
@@ -33,40 +33,5 @@ describe('EC - Date Block', () => {
     });
 
     return expect(render(optionsWithExtraClasses)).resolves.toMatchSnapshot();
-  });
-
-  test('renders correctly with time set', () => {
-    expect.assertions(1);
-
-    const optionsWithTimeSet = merge(demoData, {
-      date_time: '15:30',
-    });
-
-    return expect(render(optionsWithTimeSet)).resolves.toMatchSnapshot();
-  });
-
-  test('renders correctly with extra attributes and time set', () => {
-    expect.assertions(1);
-
-    const optionsWithExtraClassesAndTime = merge(demoData, {
-      extra_attributes: [
-        { name: 'data-test', value: 'data-test-value' },
-        { name: 'data-test-1', value: 'data-test-value-1' },
-      ],
-      date_time: '15:30',
-    });
-
-    return expect(
-      render(optionsWithExtraClassesAndTime)
-    ).resolves.toMatchSnapshot();
-  });
-
-  test('with validation enabled and missing input data returns the right warning message', () => {
-    expect.assertions(1);
-
-    const dataCompliance = { ...demoData, _compliance_: true };
-    dataCompliance.year = '';
-
-    return expect(render(dataCompliance)).resolves.toMatchSnapshot();
   });
 });
