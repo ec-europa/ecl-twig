dcb8091f17d31dfaa9ef415d469aaa790d582f34
planctus <davidepuntoferrante@gmail.com>
2020-09-02 14:22:50 +0200

    feat(site-headers): Still on the the deprecated one - front-1560

--- src/ec/packages/ec-component-site-header/adapter.js
+++ src/ec/packages/ec-component-site-header/adapter.js
@@ -13,6 +13,13 @@ const adapter = (initialData) => {
   adaptedData.language_selector = adaptedData.languageSelector;
   delete adaptedData.languageSelector;
 
+  adaptedData.language_selector.eu_category =
+    adaptedData.language_selector.overlay.categoryEu;
+  adaptedData.language_selector.non_eu_category =
+    adaptedData.language_selector.overlay.categoryNonEu;
+  delete adaptedData.language_selector.overlay.categoryEu;
+  delete adaptedData.language_selector.overlay.categoryNonEu;
+
   adaptedData.language_selector.overlay.close_label =
     adaptedData.language_selector.overlay.closeLabel;
   delete adaptedData.language_selector.overlay.closeLabel;
