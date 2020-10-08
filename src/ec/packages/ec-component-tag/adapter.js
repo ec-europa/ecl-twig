bd8d28fda7a3755172c94715693831865c625dc5
planctus <davidepuntoferrante@gmail.com>
2020-10-08 09:25:15 +0200

    feat(tag): New variant added, another deprecated - front-1709 (#571)

--- src/ec/packages/ec-component-tag/adapter.js
+++ src/ec/packages/ec-component-tag/adapter.js
@@ -4,15 +4,11 @@ const adapter = (initialData) => {
 
   if (adaptedData.dismissButtonLabel) {
     tag.default_icon_path = '/icons.svg';
-    tag.tag.type = 'removable';
     tag.tag.aria_label = adaptedData.dismissButtonLabel;
     delete tag.tag.dismissButtonLabel;
-  } else if (adaptedData.href) {
-    tag.tag.type = 'link';
+  } else if (tag.tag.type === 'link') {
     tag.tag.path = tag.tag.href;
     delete tag.tag.href;
-  } else {
-    tag.tag.type = 'button';
   }
 
   return tag;
