27f90199bcf1d996450f4628b77a2b3e3b1322b3
planctus <davidepuntoferrante@gmail.com>
2021-05-12 16:49:02 +0300

    chore: update deps (#605)

--- src/ec/packages/ec-component-footer-harmonised/demo/data--group3.js
+++ src/ec/packages/ec-component-footer-harmonised/demo/data--group3.js
@@ -11,16 +11,14 @@ const specs = {
         logo: {
           title: 'Partnership 1',
           alt: 'Partnership 1 logo',
-          src:
-            'https://inno-ecl.s3.amazonaws.com/media/examples/placeholder.svg',
+          src: 'https://inno-ecl.s3.amazonaws.com/media/examples/placeholder.svg',
         },
       },
       {
         logo: {
           title: 'Partnership 2',
           alt: 'Partnership 2 logo',
-          src:
-            'https://inno-ecl.s3.amazonaws.com/media/examples/placeholder.svg',
+          src: 'https://inno-ecl.s3.amazonaws.com/media/examples/placeholder.svg',
         },
       },
     ],
